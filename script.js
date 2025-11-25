6366a1745533b1b5e135eea7f8aefa73b6344261
Shreyesh <shreyeshpachpor06@gmail.com>
2025-11-25 15:23:08 +0530

    update

--- script.js
+++ script.js
@@ -126,16 +126,28 @@
 
 // var b = Math.floor(a);
 
-var a = Math.floor(Math.random()*100);
+// var a = Math.floor(Math.random()*100);
 
-console.log(a);
+// console.log(a);
+
+// var btn = document.querySelector('button')
+// var box = document.querySelector('#box')
+
+// btn.addEventListener('click',function(){
+//   var c1 = Math.floor(Math.random()*256
+// )
+//   console.log(c1);
+  
+// })
 
 var btn = document.querySelector('button')
 var box = document.querySelector('#box')
 
 btn.addEventListener('click',function(){
-  var c1 = Math.floor(Math.random()*256
-)
-  console.log(c1);
+ var c1 = Math.floor(Math.random()*256)
+ var c2 = Math.floor(Math.random()*256)
+ var c3 = Math.floor(Math.random()*256)
+
+  box.style.backgroundColor = `rgb(${c1},${c2},${c3})`  
   
 })
