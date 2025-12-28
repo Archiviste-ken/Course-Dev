bb3e1c1737c180ad6e948061801cd64b536d635a
Shreyesh <shreyeshpachpor06@gmail.com>
2025-12-28 18:50:28 +0530

    update

--- React/ImportExport/script.js
+++ React/ImportExport/script.js
@@ -1,3 +1,18 @@
-import user from './app.js';
+// import king from './app.js';
+
+// console.log(king);
+
+import king from './app.js';
+import skills from './test.js';
+import {user} from './test.js';
+
+import {maths, science, english} from './app.js';
+
+console.log(maths);
+console.log(science);
+console.log(english);
 
 console.log(user);
+
+console.log(king);
+console.log(skills);
