422140f6fd0e4df24e92b405d7e3e52a50bef021
Shreyesh <shreyeshpachpor06@gmail.com>
2025-12-28 18:50:14 +0530

    update

--- React/ImportExport/app.js
+++ React/ImportExport/app.js
@@ -1,4 +1,29 @@
-const user = 'Shreyesh'
+// const user = 'Shreyesh'
 
 
-export default user;
+// var skills = ['HTML', 'CSS', 'JavaScript', 'React'];
+
+// export default user;
+
+// export default skills; // only one default export allowed per file
+
+const king = {
+    name: 'Shreyesh',
+    age: 20,
+    tired: false,
+    isDeveloper: true   
+}
+
+// const queen {
+//     name: 'Ankita',
+//     age: 20,
+//     tired: true,
+//     isDeveloper: false
+// }
+
+export const maths = 100;
+export const science = 98;
+export const english = 95; 
+
+export default king;
+// export default queen; // only one default export allowed per file
