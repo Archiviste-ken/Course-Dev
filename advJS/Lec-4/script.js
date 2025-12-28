d72e90495066013a110205a30d8abd30ff036178
Shreyesh <shreyeshpachpor06@gmail.com>
2025-12-28 14:48:39 +0530

    update

--- advJS/Lec-4/script.js
+++ advJS/Lec-4/script.js
@@ -2,3 +2,23 @@
 // ek kaam ek baar mein kar payengi, baari baari kaam hota hai
 // synchronous approach
 // asynchronous approach
+
+
+// callback -> ek function jo turant nahi chalega ye chalega jab aapka koi kaam complete hoga
+
+
+// setTimeout(function() {
+//     console.log("hey");
+    
+// }, 2000);
+
+// cb -> function mein pass krte ho
+
+
+function abcd(fn){
+    fn();
+}
+
+abcd(function(){
+    console.log("hello");   
+})
