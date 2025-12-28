78cf8b4559ae36d2daf6fa41a11ee5c9a4b2f09a
Shreyesh <shreyeshpachpor06@gmail.com>
2025-12-28 14:50:18 +0530

    update

---
+++ React/Lec1/script.js
@@ -0,0 +1,54 @@
+// destructuring of array.
+
+// spread operator
+
+// let arr = [1,2,3,4,5]
+
+// let arr2 = [...arr]
+
+// var arr3 = [10,20,30,40,50]
+
+// var [a,b,c] = arr
+
+// console.log(a);
+
+
+// var arr = [1,2,3,4,5]
+
+// var a = arr[0]
+
+
+// var arr = [1,2,3,4,5]
+
+// var a = arr[0]
+// var b = arr[1]
+// var c = [...arr]
+
+
+// console.log(a);
+
+// var [x,y,z] = arr
+
+// console.log(x);]
+
+
+var arr = [1,2,3,4,5,6,7,8,9,10]
+
+// var [a,b,...c] = arr
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var arr2 = [...arr]
+
+// console.log(arr2);
+
+// arr2.push(11)
+
+// console.log(arr2);
+// console.log(arr);
+
+var [a,b,...arr2]   = arr
+
+console.log(arr2);
