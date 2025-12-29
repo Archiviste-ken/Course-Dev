704161b0770b2a57e7e5ad89d1af6d7a7849043a
Shreyesh <shreyeshpachpor06@gmail.com>
2025-12-29 17:11:07 +0530

    update

--- React/Lec2/script.js
+++ React/Lec2/script.js
@@ -1,3 +1,21 @@
 // React js - Library for building user interfaces
 
 // gsap.to("#box", {duration: 2, x: 100});
+
+
+
+// var container = document.querySelector('#container');
+
+// var root = ReactDOM.createRoot(container);  
+
+// root.render(h1);
+
+var h1 = React.createElement('h1', null, 'Hello World!');
+var h2 = React.createElement('h2', null, 'Hello from React!');
+
+
+var div = React.createElement('div',null,h1,h2)
+
+var root = ReactDOM.createRoot(document.querySelector('#container'));
+
+ root.render(h2);
