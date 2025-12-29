062bf72e3c8fc7b097226f07392ea440dfe6d11b
Shreyesh <shreyeshpachpor06@gmail.com>
2025-12-29 13:47:52 +0530

    lec2

--- React/ImportExport/app.js
+++ React/ImportExport/app.js
@@ -1,29 +1,35 @@
-// const user = 'Shreyesh'
+// // const user = 'Shreyesh'
 
 
-// var skills = ['HTML', 'CSS', 'JavaScript', 'React'];
+// // var skills = ['HTML', 'CSS', 'JavaScript', 'React'];
 
-// export default user;
+// // export default user;
 
-// export default skills; // only one default export allowed per file
+// // export default skills; // only one default export allowed per file
 
-const king = {
-    name: 'Shreyesh',
-    age: 20,
-    tired: false,
-    isDeveloper: true   
-}
-
-// const queen {
-//     name: 'Ankita',
+// const king = {
+//     name: 'Shreyesh',
 //     age: 20,
-//     tired: true,
-//     isDeveloper: false
+//     tired: false,
+//     isDeveloper: true   
 // }
 
-export const maths = 100;
-export const science = 98;
-export const english = 95; 
+// // const queen {
+// //     name: 'Ankita',
+// //     age: 20,
+// //     tired: true,
+// //     isDeveloper: false
+// // }
 
-export default king;
-// export default queen; // only one default export allowed per file
+// export const maths = 100;
+// export const science = 98;
+// export const english = 95; 
+
+// export default king;
+// // export default queen; // only one default export allowed per file
+
+
+const user = 'atomic';
+export default user;
+
+export const greeting = 'hello';
