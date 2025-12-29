1fb80f8060352ecd0d4791902ff4602630957549
Shreyesh <shreyeshpachpor06@gmail.com>
2025-12-29 13:48:13 +0530

    lec2

--- React/ImportExport/script.js
+++ React/ImportExport/script.js
@@ -2,17 +2,36 @@
 
 // console.log(king);
 
-import king from './app.js';
-import skills from './test.js';
-import {user} from './test.js';
+// import king from './app.js';
+// import skills from './test.js';
+// import {user} from './test.js';
 
-import {maths, science, english} from './app.js';
+// import {maths, science, english} from './app.js';
 
-console.log(maths);
-console.log(science);
-console.log(english);
+// console.log(maths);
+// console.log(science);
+// console.log(english);
 
-console.log(user);
+// console.log(user);
 
-console.log(king);
-console.log(skills);
+// console.log(king);
+// console.log(skills);
+
+// import hello from './app.js';
+
+// console.log(hello);
+
+// import {greeting} from './app.js';
+// console.log(greeting);
+
+function hero(){
+    console.log('Hey');  //output: Hey
+}
+
+console.log(hero()); //output: Hey undefined , why undefined? because hero function does not return anything.
+
+function hero2(){
+   console.log('Hello');
+}
+
+hero2();
