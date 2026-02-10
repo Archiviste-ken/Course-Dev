1286ca7cadf38a56d427078d6ea16e75b50c6cec
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-11 00:16:29 +0530

    added commented  notes related to the class

---
+++ Backend/Day-1/index.js
@@ -0,0 +1,17 @@
+console.log("Ohaiyo!")
+
+
+// * How to run script outside of the browser?
+
+// if you want to run the js file outside of the browser, use node <filename> in the terminal.
+
+//  node <filename>
+
+
+
+// * What are packages.
+// packages are such code which are written by other developers but has been made available publically so that the other developers can also make the use of it in their projects or anything.
+
+// -> How to install packages?
+
+// to install packages we use npm which stands for node package manager. It is a tool which helps us to install packages in our project.
