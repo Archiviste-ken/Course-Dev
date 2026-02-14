9947c6e1aad84834a5d7ecf024ccfcd134ce03b4
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-15 00:19:47 +0530

    nodemon package

--- Backend/Day-2/server.js
+++ Backend/Day-2/server.js
@@ -2,4 +2,8 @@ const express = require("express")
 
 const app = express()  // server instance create krna
 
+app.get('/kaneki',(req,res) => {
+    res.send("tank")
+})
+
 app.listen(3000)  // server start krna
