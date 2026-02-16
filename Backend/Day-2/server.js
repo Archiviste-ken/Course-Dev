f949b798ee8413769371ac4ead14425ba381dea6
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-16 19:15:23 +0530

    practice again

--- Backend/Day-2/server.js
+++ Backend/Day-2/server.js
@@ -1,9 +1,23 @@
-const express = require("express")
+// const express = require("express")// loads the express.js library into your file.
 
-const app = express()  // server instance create krna
+// const app = express()  // server instance create krna
 
-app.get('/kaneki',(req,res) => {
-    res.send("tank")
+// app.get('/kaneki',(req,res) => {
+//     res.send("tank")
+// })
+
+// app.get('/dashboard',(req,res) => {
+//     res.send("Welcome to the dashboard")
+// })
+
+// app.listen(3000)// server start krna
+
+const express = require("express") // express.js library ko load krna
+
+const app = express()// express application ka instancer create krna
+
+app.get('/kaizen',(req,res) => {
+    res.send("Welcome to kaizen")
 })
 
-app.listen(3000)  // server start krna
+app.listen(3000)
