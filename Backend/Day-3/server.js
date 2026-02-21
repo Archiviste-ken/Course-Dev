e66fbbdc99a2547ad47a557d4a8344ee862853eb
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-22 01:51:36 +0530

    did some revision to start the serve which is backed with nodemon.

--- Backend/Day-3/server.js
+++ Backend/Day-3/server.js
@@ -25,6 +25,16 @@ app.listen(3000,()=> {
 
 
 
+// const express = require('express')
+
+// const app = express()
+
+// app.get('/',(req,res) => {
+//     res.send("Hello World")
+// })
+
+// app.listen(3000)
+
 
 
 
