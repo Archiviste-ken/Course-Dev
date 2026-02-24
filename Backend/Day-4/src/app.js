20d4094bb9656ef33d5121def7a14fe41bfe5964
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-24 20:38:37 +0530

    created src/app.js express server

--- Backend/Day-4/src/app.js
+++ Backend/Day-4/src/app.js
@@ -1,6 +1,29 @@
 // server ko create krna
 // server ko config krna
 
+// config mtlb like middlewares ka use jaisa humne express ke liye use kiya thha  app.use(express.json()
 
 
-const express = require("")
+
+// const express = require("express")
+
+// const app = express()
+
+// app.get('/',(req,res) => {
+//     res.send(notes)
+// })
+
+
+// app.listen(3000)
+
+
+
+
+
+const express = require("express")
+
+const app = express() // server create hojata hai
+
+
+
+module.exports = app
