a79fcbc58a12b5abd19a2dd59ceb236e0b8f4d23
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-24 21:54:45 +0530

    update

--- Backend/Day-1/app.js
+++ Backend/Day-1/app.js
@@ -1,6 +1,14 @@
+// const express = require("express")
+
+// const app = express() // sever created, sever create kr chuke hai.
+
+// app.listen(3000) // server ko start krta hai.
+
+
+
+
 const express = require("express")
 
+const app = express()
 
-const app = express() // sever created, sever create kr chuke hai.
-
-app.listen(3000) // server ko start krta hai.
+app.listen(3000)
