1df164a3643897f4ed1a7b5331a9bbf37ff0528b
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-28 00:12:51 +0530

    params, database

--- Backend/Day-5/src/app.js
+++ Backend/Day-5/src/app.js
@@ -20,17 +20,29 @@ app.get("/notes",(req,res)=>{
 app.post('/notes',(req,res) =>{
     console.log(req.body);
     notes.push(req.body)
-    res.send("note created")
+    // res.send("note created")
+
+    res.status(201).json({
+        message: "Note created successfully"
+    })
     
 })
 
 
+app.delete("/notes/:index",(req,res)=>{
+    delete notes[req.params.index]
+    res.status(204).json({
+        message: "Note deleted successfully"
+    })
+})
 
 
+app.patch("/notes/:index", (req,res) =>{
+    notes[req.params.index].description = req.body.description
 
-
-
-
-
+    res.status(200).json({
+        message: "Notes updated"
+    })
+})
 
 module.exports = app
