a145ce363e49c5444eedd5e9673a5c47b35a1486
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-01 16:48:24 +0530

    Remove commented-out code in app.js to enhance clarity and maintainability

--- Backend/Day-7/src/app.js
+++ Backend/Day-7/src/app.js
@@ -3,8 +3,14 @@ server ko create krna
 server ko configure krna
 */
 
+// const express = require("express")
+// const app = express()
+
+
+// module.exports = app
+
+
 const express = require("express")
 const app = express()
-
 
 module.exports = app
