104256208bf2c194785b5ce1a61e850c74ee596c
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-01 16:48:16 +0530

    Fix connection string in database.js to ensure proper MongoDB connection

--- Backend/Day-7/config/database.js
+++ Backend/Day-7/config/database.js
@@ -1,8 +1,21 @@
+// const mongoose = require("mongoose")
+
+
+// function connectDB(){
+//     mongoose.connect("mongodb+srv://gojosatoru6862_db_user:Z5n68iwcSQRiGfJo@cluster0.y8rhtxn.mongodb.net/Day-7").then(()=>{
+//         console.log("connected to the database successfully");
+//     })
+// }
+
+
+// module.exports = connectDB
+
+
 const mongoose = require("mongoose")
 
 
 function connectDB(){
-    mongoose.connect("mongodb+srv://gojosatoru6862_db_user:Z5n68iwcSQRiGfJo@cluster0.y8rhtxn.mongodb.net/Day-7").then(()=>{
+    mongoose.connect("mongodb+srv://gojosatoru6862_db_user:Z5n68iwcSQRiGfJo@cluster0.y8rhtxn.mongodb.net//Day-7").then(()=>{
         console.log("connected to the database successfully");
     })
 }
