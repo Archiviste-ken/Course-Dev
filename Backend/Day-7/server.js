458ad0d3d854942410f1e450d1719f5b2db64a4e
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-01 16:48:12 +0530

    Refactor server.js to clean up commented code and improve readability

--- Backend/Day-7/server.js
+++ Backend/Day-7/server.js
@@ -7,17 +7,29 @@ database se connect karna
 */
 
 
-const app = require("./src/app")
-const mongoose = require("mongoose")
+// const app = require("./src/app")
+// const mongoose = require("mongoose")
+// const connectDB = require("./config/database")
+
+
+
+
+
+// connectDB()
+
+// app.listen(3000,()=>{
+//     console.log("server is now running on the port 3000");
+    
+// })
+
+
+
+const app  = require("./src/app")
 const connectDB = require("./config/database")
-
-
-
-
 
 connectDB()
 
+
 app.listen(3000,()=>{
     console.log("server is now running on the port 3000");
-    
 })
