b541d419ccbb656144c83dd344b21194819c74cb
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-01 16:40:05 +0530

    Add server.js to initialize server and connect to the database

---
+++ Backend/Day-7/server.js
@@ -0,0 +1,23 @@
+/*
+
+
+server ko start krna
+database se connect karna
+
+*/
+
+
+const app = require("./src/app")
+const mongoose = require("mongoose")
+const connectDB = require("./config/database")
+
+
+
+
+
+connectDB()
+
+app.listen(3000,()=>{
+    console.log("server is now running on the port 3000");
+    
+})
