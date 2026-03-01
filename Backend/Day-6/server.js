7f58bd1099f1d4aa8a6c26ab7b3b386b27e64b4e
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-01 11:57:52 +0530

    Refactor database connection to use promise and log connection status

--- Backend/Day-6/server.js
+++ Backend/Day-6/server.js
@@ -10,10 +10,15 @@ const mongoose = require("mongoose")
 
 function connectToDB(){
 
-    mongoose.connect("mongodb+srv://gojosatoru6862_db_user:Z5n68iwcSQRiGfJo@cluster0.y8rhtxn.mongodb.net/Day-6")
+    mongoose.connect("mongodb+srv://gojosatoru6862_db_user:Z5n68iwcSQRiGfJo@cluster0.y8rhtxn.mongodb.net/Day-6").then(()=>{
+        console.log("Connected to database");
+        
+    })
     // mongoose.connect this method connects express server to the mongodb database.
 //this uri connects mongoose to the database cluster and the specific database we want to use. It includes the username, password, and the name of the database we want to connect to.
 }
+
+connectToDB()
 
 
 app.listen(3000,()=>{
