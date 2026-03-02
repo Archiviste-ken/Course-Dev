78af11e60034b554cfbf2f5943ee4cc71a1c396a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-02 17:04:43 +0530

    Add notes.model.js with initial note schema and model definition

---
+++ Backend/Day-7/models/notes.model.js
@@ -0,0 +1,84 @@
+// const mongooose = require("mongoose")
+
+
+// const noteSchema = new mongoose.Schema({
+//     title: String,
+//     description: String,
+// })
+
+
+// const noteModel = mongoose.model("notes",noteSchema)
+
+
+
+// const mongoose = require("mongoose")
+
+// const noteSchema = new mongoose.Schema({
+//     title: String,
+//     description: String
+// })
+
+
+// const noteModel = mongoose.model("notes",noteSchema)
+
+
+// module.exports = noteModel
+
+
+
+// const mongoose = require("mongoose")
+
+// const noteSchema =new mongooose.Schema({
+//     title: String,
+//     description: String
+// })
+
+// const noteModel = mongoose.model("notes",noteSchema)
+
+
+
+// const mongoose = require("mongoose")
+
+
+// const noteSchema = new mongoose.Schema({
+//     title: String,
+//     description: String
+// })
+
+
+// const noteModel = mongoose.model("notes",noteSchema)
+
+
+
+
+// const mongoose = require("mongoose")
+
+// new mongoose.Schema({
+//     title: String,
+//     description: String
+// })
+
+// const noteModel = mongoose.model("notes",notesSchema)
+
+
+
+
+const mongoose = require("mongoose")
+
+
+const noteSchema = new mongoose.Schema({
+    title: String,
+    description: String
+})
+
+
+const noteModel = mongoose.model("notes",notesSchema)
+
+
+
+
+
+
+
+
+
