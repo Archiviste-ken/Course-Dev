c9f93c4bfe0ff2443d17d7e671c8faadf4c40bc3
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-02 17:04:34 +0530

    Refactor database.js to improve connection string and clean up commented code

--- Backend/Day-7/config/database.js
+++ Backend/Day-7/config/database.js
@@ -11,14 +11,30 @@
 // module.exports = connectDB
 
 
+// const mongoose = require("mongoose")
+
+
+// function connectDB(){
+//     mongoose.connect("mongodb+srv://gojosatoru6862_db_user:Z5n68iwcSQRiGfJo@cluster0.y8rhtxn.mongodb.net//Day-7").then(()=>{
+//         console.log("connected to the database successfully");
+//     })
+// }
+
+
+// module.exports = connectDB
+
+
+
 const mongoose = require("mongoose")
 
 
+
 function connectDB(){
-    mongoose.connect("mongodb+srv://gojosatoru6862_db_user:Z5n68iwcSQRiGfJo@cluster0.y8rhtxn.mongodb.net//Day-7").then(()=>{
-        console.log("connected to the database successfully");
+    mongoose.connect("mongodb+srv://gojosatoru6862_db_user:Z5n68iwcSQRiGfJo@cluster0.y8rhtxn.mongodb.net/Day-7").then(()=>{
+        console.log("connected successfully");
+        
     })
 }
 
 
-module.exports = connectDB
+Module.exports = connectDB
