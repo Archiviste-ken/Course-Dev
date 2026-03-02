98cdb92287df019d6e4b39b313ca465bc463b6f8
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-02 23:04:27 +0530

    Add GET endpoint to fetch all notes and update note model schema

--- Backend/Day-7/src/app.js
+++ Backend/Day-7/src/app.js
@@ -56,4 +56,23 @@ app.post("/notes",async (req,res)=>{
 })
 
 
+/**
+ * - GET/ notes
+ * fetch all the notes data
+ */
+
+app.get("/notes", async (req,res)=>{
+
+     const notes = await noteModel.find()
+
+
+     res.status(200).json({
+        message: "Notes fetched successfully",
+        notes
+     })
+})
+
+
+
+
 module.exports = app
