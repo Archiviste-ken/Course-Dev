ca46316f8474e4263d8c3fbe22382aada9605681
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-04 22:03:57 +0530

    Fix database connection function name in server.js

--- Backend/Day-9/Backend/server.js
+++ Backend/Day-9/Backend/server.js
@@ -1,15 +1,15 @@
+/**
+ * server ko start krna
+ * database se connect krna
+ */
 require('dotenv').config()
 const app = require("./src/app")
-const connectDB = require("./src/config/database")
+const connectToDB = require("./src/config/database")
 
 
-connectDB()
+connectToDB()
 
 
-
-
-
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000");
-    
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
 })
