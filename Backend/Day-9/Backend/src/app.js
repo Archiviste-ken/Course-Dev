a48b692d10407aa930b65db7519586af6e0c3d79
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-04 20:27:12 +0530

    Implement CRUD operations for notes in app.js

--- Backend/Day-9/Backend/src/app.js
+++ Backend/Day-9/Backend/src/app.js
@@ -1,6 +1,67 @@
 const express = require("express")
-
+const noteModel = require("./models/note.model")
 const app = express()
+
+app.use(express.json())
+
+
+/**
+ * - POST /api/notes
+ * - create new note and save data in mongoDB
+ * - req.body = {title,description}
+ */
+
+app.post("/api/notes",async (req,res)=>{
+        const {title,description} = req.body
+
+        const note = await noteModel.create({
+            title,description
+        })
+
+        res.status(201).json({
+            message: "note created",
+            note
+        })
+    })
+
+
+    /**
+     * - GET /api/notes
+     * - fetch all the data from mongodb and send them in the response
+     */
+
+
+app.get("/api/notes",async (req,res)=>{
+   const notes = await noteModel.find()
+
+   res.status(200).json({
+    message: "Notes fetched successfully.",
+    notes
+   })
+})
+
+/**
+ * - DELETE /api/notes/:id
+ * - Delete notes with the id from req.params
+ */
+
+
+app.delete('/api/notes/:id', async (req,res) => {
+    const id = req.params.id
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message: "Note deleted successfully."
+    })
+    
+})
+
+
+module.exports = app
+
+
+        
 
 
 module.exports = app
