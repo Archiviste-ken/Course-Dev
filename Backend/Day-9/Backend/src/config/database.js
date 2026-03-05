4c41a6371a5841f0d2512322870ba488f2136c24
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-05 13:56:04 +0530

    Refactor database.js to clean up commented code and standardize database connection function

--- Backend/Day-9/Backend/src/config/database.js
+++ Backend/Day-9/Backend/src/config/database.js
@@ -1,12 +1,24 @@
+// const mongoose = require("mongoose")
+
+
+// function connectToDB(){
+//     mongoose.connect(process.env.MONGO_URI).then(()=>{
+//         console.log("Connected to DB");
+        
+//     })
+// }
+
+
+// module.exports = connectToDB
+
+
 const mongoose = require("mongoose")
 
-
 function connectDB(){
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
-        console.log("Connected to DB");
-        
-    })
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connect to DB");
+    
+})
 }
-
 
 module.exports = connectDB
