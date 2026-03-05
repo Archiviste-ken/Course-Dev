dfa7da5a61f138eedbee9afab3ebbf998d94cd70
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-05 13:55:48 +0530

    Refactor server.js to clean up commented code and standardize database connection function name

--- Backend/Day-9/Backend/server.js
+++ Backend/Day-9/Backend/server.js
@@ -2,14 +2,29 @@
  * server ko start krna
  * database se connect krna
  */
+// require('dotenv').config()
+// const app = require("./src/app")
+// const connectToDB = require("./src/config/database")
+
+
+// connectToDB()
+
+
+// app.listen(3000, () => {
+//     console.log("server is running on port 3000")
+// })
+
 require('dotenv').config()
 const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+const connectDB = require("./src/config/database")
 
 
-connectToDB()
+connectDB()
 
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000")
+
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+    
 })
