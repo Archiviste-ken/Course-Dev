79aa709a45b40ca23ef820ee884502ed4f63cb38
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-07 13:31:28 +0530

    Refactor App component to remove hardcoded notes and implement fetchNotes function for dynamic data retrieval

--- Backend/Day-9/Frontend/src/App.jsx
+++ Backend/Day-9/Frontend/src/App.jsx
@@ -1,50 +1,51 @@
-import { useState } from 'react'
+import { useState,useEffect } from "react"
+// import { useState } from 'react'
 import axios from "axios"
 
 
 function App() {
-      const [notes,setNotes ] = useState([
-        {
-          title: "test title 1",
-          description: "test description"
-        },
-         {
-          title: "test title 2",
-          description: "test description"
-        },
-         {
-          title: "test title 3",
-          description: "test description"
-        },
-         {
-          title: "test title 4",
-          description: "test description"
-        },
-      ])
-
-      axios.get('http://localhost:3000/api/notes').then((res)=>{
+      const [notes,setNotes ] = useState([])
+      console.log("hello");
+      
+    function fetchNotes(){
+      
+ axios.get('http://localhost:3000/api/notes')
+    .then (res=>{
         setNotes(res.data.notes)
-        console.log(res.data);
+        // console.log(res.data);
         
-      })
+ })
+    }
+
+      useEffect(()=>{
+
+        fetchNotes()
+
+      },[])
+     
+     
 
   return (
     
    <>
+
+  <form className='note-create-form'>
+    <input type="text" placeholder = "Enter title" />
+    <input type="text" placeholder = "Enter description" />
+    <button>Create note</button>
+  </form>
+
     <div className ="notes">
       {
-        notes.map(note=>{
-    return <div className="note">
+      notes.map(note=>{
+        return <div className="note">
         <h1>{note.title}</h1>
         <p>{note.description}</p>
       </div>
-
-        })
-      }
-    
-      
-      </div>   
-   </>
+      })
+     }
+    </div>   
+  </>
 
    
   )
