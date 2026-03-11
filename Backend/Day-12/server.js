638818e3aeab1a881f5bc2460b57843ee608e024
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-11 23:17:43 +0530

    Initialize server with dotenv configuration and database connection

---
+++ Backend/Day-12/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const app = require("./src/app");
+const connectDB = require("./src/config/database")
+
+
+connectDB()
+
+app.listen(3000, (req, res) => {
+  console.log("server is now running on the port 3000");
+});
