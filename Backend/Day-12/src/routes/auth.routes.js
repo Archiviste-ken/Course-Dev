8cd67d252f000a836dcd6694760e9c9f7b80f98a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-12 14:42:19 +0530

    Fix auth route definition and update user registration response

--- Backend/Day-12/src/routes/auth.routes.js
+++ Backend/Day-12/src/routes/auth.routes.js
@@ -1,6 +1,17 @@
 const express = require("express")
+const userModel = require(../models/user.model)
 
-const authRouter = express.Router()
+const authRoutes = express.Router()
 
+authRouter.post("/register",async (req,res)=>{
+    const {email,name,password} = req.body
+        const user = await userModel.create({
+            email,password, name
+        })
+
+        res.status(201).json({
+            message: "user register".user
+        })
+})
 
 module.exports = authRouter
