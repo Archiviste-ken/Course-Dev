bbc4f7ac073b4ef08aaac830563792614479422a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-14 23:49:28 +0530

    Enhance comments for clarity in auth.routes.js

--- Backend/Day-12/src/routes/auth.routes.js
+++ Backend/Day-12/src/routes/auth.routes.js
@@ -2,8 +2,15 @@ const express = require("express")
 const userModel = require("../models/user.models")
 const jwt = require("jsonwebtoken")
 
-const authRouter = express.Router()
+const authRouter = express.Router() // this is the router for the authentication related routes, we will use this router in our app.js file and prefix it with /api/auth, so all the routes defined in this file will be prefixed with /api/auth.
 
+// if we want to create api routes in other files other than app.js, we need to create a router for that file and then use that router in app.js file, this way we can keep our code organized and modular.
+
+
+/**
+ * /api/route/register
+ * 
+ */
 authRouter.post("/register", async (req,res)=> {
     const {email,name,password} = req.body
 
