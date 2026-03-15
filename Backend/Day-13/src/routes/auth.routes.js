2899c07a8ef3e9f5b7e2b0e26995776067eb8ecd
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-15 20:13:08 +0530

    Refactor registration and login routes to include JWT token generation and improve response status codes

--- Backend/Day-13/src/routes/auth.routes.js
+++ Backend/Day-13/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userModel = require("../models/user.models");
 const jwt = require("jsonwebtoken");
-
+const crypto = require("crypto")
 const authRouter = express.Router(); // this is the router for the authentication related routes, we will use this router in our app.js file and prefix it with /api/auth, so all the routes defined in this file will be prefixed with /api/auth.
 
 // if we want to create api routes in other files other than app.js, we need to create a router for that file and then use that router in app.js file, this way we can keep our code organized and modular.
@@ -17,36 +17,38 @@ authRouter.post("/register", async (req, res) => {
   const isNameAlreadyExists = await userModel.findOne({ name });
 
   if (isUserAlreadyExists) {
-    return res.status(400).json({
+    return res.status(409).json({
       message: "User already exists with this email address",
     });
   }
   if (isNameAlreadyExists) {
-    return res.status(400).json({
+    return res.status(409).json({
       message: "Name already exists",
     });
   }
+
+  const hash = crypto.createHash("md5").update(password).digest("hex")
   const user = await userModel.create({
     email,
-    password,
+    password : hash,
     name,
-  });
+  })
 
-//   const token = jwt.sign(
-//     {
-//       // JWt = JSON Web Token. When the user logs in or registers, the server gives them this token so they don't need to log in again on every request.
-//       id: user._id,
-//       email: user.email,
-//     },
-//     process.env.JWT_SECRET,
-//   );
+  //   const token = jwt.sign(
+  //     {
+  //       // JWt = JSON Web Token. When the user logs in or registers, the server gives them this token so they don't need to log in again on every request.
+  //       id: user._id,
+  //       email: user.email,
+  //     },
+  //     process.env.JWT_SECRET,
+  //   );
 
-//   res.cookie("jwt_token", token);
-//   res.status(201).json({
-//     message: "user register",
-//     user,
-//     token,
-//   });
+  //   res.cookie("jwt_token", token);
+  //   res.status(201).json({
+  //     message: "user register",
+  //     user,
+  //     token,
+  //   });
 
   //     const token = jwt.sign({
   //         id: user._id,
@@ -59,31 +61,88 @@ authRouter.post("/register", async (req, res) => {
   //         message: "user register", user , token
   //     })
 
-//       const token = jwt.sign({
-//           id: user._id,
-//           email: user.email,
-//       },
-//       process.env.JWT_SECRET,
-//   );
+  //       const token = jwt.sign({
+  //           id: user._id,
+  //           email: user.email,
+  //       },
+  //       process.env.JWT_SECRET,
+  //   );
 
-//       res.cookie("jwt_token", token);
-//           res.status(201).json({
-//               message: "user register", user , token
-//           });
+  //       res.cookie("jwt_token", token);
+  //           res.status(201).json({
+  //               message: "user register", user , token
+  //           });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  )
+
+  res.cookie("jwt_token", token);
+  res.status(201).json({
+    message: "user register",
+    user,
+    token,
+  })
+
+  
+})
+
+/**
+   * /api/auth/protected
+   */
+
+/**
+ *  controller function
+ */
+
+
+  authRouter.post("/protected", (req, res) => {
+    console.log(req.cookies);
+
+    res.status(201).json({
+      message: "This is a protected route",
+    })
+  }
+)
+
+authRouter.post("/login", async (req,res)=>{        // call back function
+            const {email, password} = req.body
+            const user = await userModel.findOne({email})
+            if(!user){
+              return res.status(404).json({
+                message: "User not found with this email address"
+              })
+            }
+        const isPasswordMatched = user.password === crypto.createHash("md5").update(password).digest("hex")
+
+        if(!isPasswordMatched){
+          return res.status(401).json({
+            message: "Invalid Password"
+          })
+        }
+
 
         const token = jwt.sign({
-            id: user._id,
-            email: user.email,
+          id: user._id,
+        
         },
-        process.env.JWT_SECRET,  
-    );
-    
-    res.cookie("jwt_token", token);
-            res.status(201).json({
-                message: "user register",
-                user,
-                token
-            });
-});
+        process.env.JWT_SECRET
+      )
+
+      res.cookie("jwt_token",token)
+
+
+      res.status(200).json({
+        message: "User logged in",
+        user,
+      })
+})
+
+
+
 
 module.exports = authRouter;
