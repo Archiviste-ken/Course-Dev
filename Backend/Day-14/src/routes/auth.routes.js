641020cd20367dbe54db05145ade8f76faab1278
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-16 22:14:46 +0530

    feat(auth.routes.js): add initial registration route with request handling

---
+++ Backend/Day-14/src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const authRouter = express.Router()
+
+
+authRouter.post("/register", async (req, res)=>{
+            const {username, email, password} = req.body
+            
+})
