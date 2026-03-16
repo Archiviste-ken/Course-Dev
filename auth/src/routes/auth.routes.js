fdb559c1ff0099bba5be69dc3882183ec62c5da3
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-16 11:13:35 +0530

    feat(auth.routes): implement user registration endpoint with hashed password and JWT token

--- auth/src/routes/auth.routes.js
+++ auth/src/routes/auth.routes.js
@@ -1,9 +1,54 @@
 const express = require("express")
-const userModel = require("./src/model/user.model.js")
+const userModel = require("../model/user.model")
 const authRouter = express.Router();
+const crypto = require("crypto")
+
+/**
+ * POST /api/auth/register
+ */
+
+authRouter.post("/register",async(req,res)=>{
+    const { name, email, password } = req.body
+
+    const isUserExists = await userModel.findOne({email})
+
+    if(isUserExists){
+        return res.status(409).json({
+            message: " User Already exists"
+        })
+    }
+
+    const user = await userModel.create({
+        name,
+        email,
+        password: crypto.createHash("sha256").update(password).digest("hex")
+    })
+
+
+    const token = jwt.sign({
+        id: user._id,
+    },
+    process.env.JWT_SECRET, { expiresIn: " 1h"})
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message: "User Registered Successfully",
+        user: {
+            name: user.name,
+            email: user.email
+        }
+
+    })
+
+    
+})
 
 
 
+
+
+// authRouter.post("/login")
 
 module.exports = authRouter;
 
