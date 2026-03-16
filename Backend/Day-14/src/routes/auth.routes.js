477b2a7356ba7fc9f871898c30a668e7c47958a3
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-17 00:29:23 +0530

    feat(auth.routes.js): implement email existence check in registration route

--- Backend/Day-14/src/routes/auth.routes.js
+++ Backend/Day-14/src/routes/auth.routes.js
@@ -5,7 +5,13 @@ const authRouter = express.Router()
 
 authRouter.post("/register", async (req, res)=>{
             const {username, email, password} = req.body
-
-
             const isUserExistsByEmail =  await userModel.findOne({email})
-})
+
+
+    if(isUserExistsByEmail){
+    return res.status(400).json({
+        message: "Email already exists"
+                })
+            
+            
+}
