423ccfaa41beb18aaa3f2478433c0b8ff534d510
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-17 16:33:21 +0530

    fix(app.js): reorganize middleware setup for clarity and consistency

--- Backend/Day-14/src/app.js
+++ Backend/Day-14/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
-
+const cookieParser = require("cookie-parser")
 const app = express()
+authRouter = require("./routes/auth.routes")
 
+app.use("/api/auth", authRouter)
 app.use(express.json())
 
-
+app.use(cookieParser())
 
 module.exports = app
