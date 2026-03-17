3e143596bccb124744067c7e88462a0e48397378
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-17 15:06:40 +0530

    refactor(auth.routes.js): remove commented-out email and username existence checks for cleaner code

--- Backend/Day-14/src/routes/auth.routes.js
+++ Backend/Day-14/src/routes/auth.routes.js
@@ -5,21 +5,42 @@ const authRouter = express.Router()
 
 authRouter.post("/register", async (req, res)=>{
 const {username, email, password , bio , profileImage} = req.body
-const isUserExistsByEmail =  await userModel.findOne({email})
+// const isUserExistsByEmail =  await userModel.findOne({email})
 
 
-    if(isUserExistsByEmail){
-    return res.status(400).json({
-        message: "Email already exists"
-                })}
+//     if(isUserExistsByEmail){
+//     return res.status(400).json({
+//         message: "Email already exists"
+//                 })}
 
-        const isUserExistsByUsername = await userModel.findOne({username})
+//         const isUserExistsByUsername = await userModel.findOne({username})
 
-        if(isUserExistsByUsername){
-            return res.status(400).json({
-                message: "Username already exists"
-            })
-        }
+//         if(isUserExistsByUsername){
+//             return res.status(400).json({
+//                 message: "Username already exists"
+//             })
+//         }
+
+
+
+
+
+const isUserAlreadyExists = await userModel.findOne({
+    $or: [
+        {email},
+        {username}
+    ]
+})
+
+
+
+
+
+
+
+
+
+
 
 })
 
