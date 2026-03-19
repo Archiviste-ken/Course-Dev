41e65f4346edfecfcdc7889ec97916fcc4e388a3
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-19 07:26:39 +0530

    feat(post): define post schema with caption and imgUrl fields

--- Backend/Day-15/src/model/post.model.js
+++ Backend/Day-15/src/model/post.model.js
@@ -1,5 +1,22 @@
 const mongoose = require("mongoose")
 
 const postSchema = new mongoose.schema({
-    
+    caption:{
+        type: String,
+        default: ""
+    },
+
+    imgUrl:{
+        type: String,
+        required: [ true, " imgUrl is require for creating an post"]
+    }
 })
+
+
+
+
+const postModel = mongoose.model("posts", postSchema)
+
+
+
+module.exports = postModel
