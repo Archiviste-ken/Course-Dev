209d1bc9790baed635dcfb0798d41b4478631007
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-23 23:31:15 +0530

    feat(post): implement create post controller with initial logging

---
+++ Backend/Day-15/src/controllers/post.controller.js
@@ -0,0 +1,14 @@
+const postModel = require("../model/post.model")
+
+
+async function creatPostController(req,res){
+
+    console.log(req.body)
+
+}
+
+
+
+module.exports = {
+    creatPostController
+}
