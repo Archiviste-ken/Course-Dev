df00d4c15530f411084ee1992268d050be0c3550
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-23 23:31:33 +0530

    feat(routes): add post creation route to handle new posts

--- Backend/Day-15/src/routes/post.routes.js
+++ Backend/Day-15/src/routes/post.routes.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const postRouter = express.Router()
+const postController = require("../controllers/post.controller")
 
 
 /**
  * POST /api/posts
  * req.body = { caption, image-file}
  */
+
+
+
+postRouter.post("/",postController.creatPostController)
+
+
+
+module.exports = postRouter
