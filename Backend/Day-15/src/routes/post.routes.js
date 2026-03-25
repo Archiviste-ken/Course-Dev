4fc16cffc61283be00876dfe392c2a847230a0eb
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 14:07:32 +0530

    fix(post): add multer middleware for image upload in create post route

--- Backend/Day-15/src/routes/post.routes.js
+++ Backend/Day-15/src/routes/post.routes.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const postRouter = express.Router()
 const postController = require("../controllers/post.controller")
+const multer = require("multer") // multer is a middleware for handling multipart/form-data, which is primarily used for uploading files.
+const upload = multer({storage: multer.memoryStorage()}) // Configure multer to store uploaded files in memory. This means that the files will not be saved to disk, but will be available as a buffer in the request object. This is useful for processing files directly in memory without needing to save them to the server's filesystem.
 
 
 /**
@@ -10,7 +12,7 @@ const postController = require("../controllers/post.controller")
 
 
 
-postRouter.post("/",postController.creatPostController)
+postRouter.post("/", upload.single("image") ,postController.creatPostController)
 
 
 
