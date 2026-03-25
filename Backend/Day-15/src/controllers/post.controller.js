a831cb9c31b56c7b0ed82f6d45031c93d207e613
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 18:26:31 +0530

    fix(post): enable JWT verification and enhance post creation logic

--- Backend/Day-15/src/controllers/post.controller.js
+++ Backend/Day-15/src/controllers/post.controller.js
@@ -1,7 +1,7 @@
 const postModel = require("../model/post.model")
 const ImageKit = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
-// const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
 console.log("ENV:", process.env.IMAGEKIT_PRIVATE_KEY);
 
@@ -12,17 +12,46 @@ const imagekit = new ImageKit({
 
 async function createPostController(req, res) {
     console.log(req.body, req.file)
+ console.log("cookies:", req.cookies);
+    const token = req.cookies.token
+    
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+    let decoded = null;
+    try{ decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+   
+    console.log("decoded:", decoded);
+
 
    const file = await imagekit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), 'file'),
-    fileName: "Test"
+       fileName: "Test",
+         folder: "cohort-2-insta-clone"
    })
 
-   res.send(file)
+
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+    
+
+    res.status(201).json({
+        message: "Post created successfully",
+        post
+    })
+
 }
-
-
-
 module.exports = {
     createPostController
 }
