32567844519814612fd82fefe0f006a3cd8ec2f9
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 14:07:05 +0530

    fix(post): integrate ImageKit for image handling in create post controller

--- Backend/Day-15/src/controllers/post.controller.js
+++ Backend/Day-15/src/controllers/post.controller.js
@@ -1,9 +1,17 @@
 const postModel = require("../model/post.model")
+const imageKit = require("@imagekit/nodejs")
+
+
+
+const imagekit = ImageKit({
+    privatekey: process.env.IMAGEKIT_PRIVATEKEY
+})
+
 
 
 async function creatPostController(req,res){
 
-    console.log(req.body)
+    console.log(req.body, req.file)
     
 }
 
