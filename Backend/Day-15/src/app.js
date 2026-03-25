4cad061294f24414fa4dd50712c9768281c91387
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 18:26:19 +0530

    fix(app): reorder middleware and improve code organization

--- Backend/Day-15/src/app.js
+++ Backend/Day-15/src/app.js
@@ -1,15 +1,16 @@
 const express = require("express")
 require("dotenv").config();
 const cookieParser = require("cookie-parser")
+
 const app = express()
+
 const postRouter = require("./routes/post.routes")
+const authRouter = require("./routes/auth.routes")
+
 app.use(express.json())
-authRouter = require("./routes/auth.routes")
+app.use(cookieParser()); // ✅ MOVE THIS UP
 
 app.use("/api/auth", authRouter)
-app.use("/api/posts",postRouter)
-
-
-app.use(cookieParser())
+app.use("/api/posts", postRouter)
 
 module.exports = app
