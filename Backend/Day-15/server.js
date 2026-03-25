0e72ee594abbf9142d9b804f0004f07da550bbbf
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 15:30:52 +0530

    fix(server): format code for consistency and readability

--- Backend/Day-15/server.js
+++ Backend/Day-15/server.js
@@ -1,8 +1,8 @@
-const app = require("./src/app")
-require("dotenv").config()
-const connectDB = require("./src/config/database.js")
-connectDB()
+const app = require("./src/app");
+require("dotenv").config();
+const connectDB = require("./src/config/database.js");
+connectDB();
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
