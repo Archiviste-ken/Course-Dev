0ac0404c4fda4bb1be49023fef1f0a0c572509f1
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 18:26:10 +0530

    fix(server): add newline at end of file for consistency

--- Backend/Day-15/server.js
+++ Backend/Day-15/server.js
@@ -1,8 +1,10 @@
 const app = require("./src/app");
 require("dotenv").config();
+
 const connectDB = require("./src/config/database.js");
 connectDB();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
