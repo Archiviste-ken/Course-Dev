bac98f1aa98ca12fc0f6958fde9af52c6059e130
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 15:31:16 +0530

    fix(app): add dotenv configuration for environment variables

--- Backend/Day-15/src/app.js
+++ Backend/Day-15/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+require("dotenv").config();
 const cookieParser = require("cookie-parser")
 const app = express()
 const postRouter = require("./routes/post.routes")
