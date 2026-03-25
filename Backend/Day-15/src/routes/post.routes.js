0b39961ede6f7a112624ddd67d50bc352c5295fd
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 15:31:04 +0530

    fix(post): correct import and usage of createPostController in post route

--- Backend/Day-15/src/routes/post.routes.js
+++ Backend/Day-15/src/routes/post.routes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const postRouter = express.Router()
-const postController = require("../controllers/post.controller")
+const { createPostController } = require("../controllers/post.controller")
 const multer = require("multer") // multer is a middleware for handling multipart/form-data, which is primarily used for uploading files.
 const upload = multer({storage: multer.memoryStorage()}) // Configure multer to store uploaded files in memory. This means that the files will not be saved to disk, but will be available as a buffer in the request object. This is useful for processing files directly in memory without needing to save them to the server's filesystem.
 
@@ -12,7 +12,7 @@ const upload = multer({storage: multer.memoryStorage()}) // Configure multer to 
 
 
 
-postRouter.post("/", upload.single("image") ,postController.creatPostController)
+postRouter.post("/", upload.single("image"), createPostController)
 
 
 
