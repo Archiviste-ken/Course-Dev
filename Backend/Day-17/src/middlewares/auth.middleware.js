7e868c9c893c4f8f35f6c06858cae09412ba1da4
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-26 14:01:20 +0530

    feat(auth): implement user identification middleware with JWT verification

---
+++ Backend/Day-17/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+
+const jwt = require("jsonwebtoken");
+
+
+async function identifyUser(req, res, next) {
+    const token = req.cookies.token;  
+    if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized Access",
+    });
+  }
+
+  let decoded = null;
+
+  const token = req.cookies.token;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+}
+
+
+module.exports = {
+    identifyUser
+}
