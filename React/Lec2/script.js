2bc30d381e9ebe3f8cfdee0bcacd3905c427a06b
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-26 21:28:04 +0530

    feat: initialize React app with Vite, add main components and styles
    - Created main application structure with App.jsx and main.jsx
    - Added CSS styles in App.css and index.css for layout and theming
    - Included assets: hero image, React and Vite logos
    - Configured Vite for React support
    - Implemented a counter button with state management

--- React/Lec2/script.js
+++ React/Lec2/script.js
@@ -39,7 +39,11 @@
 
 
 var h1 = React.createElement('h1',null, 'Mai hoon Sp!');
-var h2 = React.createElement('h2', null, 'Mai hoon Sp!');
+
+// var h1 = React.createElement('h1',{id: 'parent'}, 'Mai hoon Sp!'); 
+// also can be written like this. here we are passing an object as the second argument which contains the attributes of the element. in this case we are passing id attribute with value 'parent'. we can pass any attribute in this object like className, style etc. and we can also pass multiple attributes in this object.
+
+var h2 = React.createElement('h2', null, 'Mai hoon Sp!');  // nul is for attributes, we can pass any attribute in place of null. like id, className, style etc. and the third argument is for content. we can pass any content in place of 'Mai hoon Sp!' like number, boolean, array, object etc. but if we pass an array then it will be treated as children and it will render all the elements in the array as children of the parent element.
 var div = React.createElement('div',null , [h1 , h2])
 
 var  container = document.querySelector('#container');
