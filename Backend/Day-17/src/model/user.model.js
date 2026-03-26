039b87b071697a53bf6ebb33678212158a420241
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-26 06:41:03 +0530

    feat(user): add user model with schema for username, email, password, bio, followers, and profile image

---
+++ Backend/Day-17/src/model/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, "Username already exists"],
+        required: [true, "Username is required"],
+    },
+
+    email: {
+        type: String,
+        unique: [ true, " Email already exists"],
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]    
+},
+    bio: String,
+    followers: String,
+    profile_image: {
+        type: String,
+        default: "https://i.ytimg.com/vi/JjrczzikEo4/mqdefault.jpg"
+    },
+})
+
+
+const userModel = mongoose.model("users", userSchema )
+
+
+
+module.exports = userModel
