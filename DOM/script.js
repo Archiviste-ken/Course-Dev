7ef1a9006d6342c9795a499125cca8bf8caeb6e4
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-27 10:17:47 +0530

    feat: implement initial DOM manipulation in script.js

---
+++ DOM/script.js
@@ -0,0 +1,26 @@
+
+
+
+var h1 = document.querySelector('h1') // FIRST PILLAR
+
+console.log(h1);
+
+h1.innerHTML = 'Hello World' // SECOND PILLAR
+
+h1.style.color = 'cyan' // THIRD PILLAR
+
+h1.style.backgroundColor = 'red'
+
+
+h1.addEventListener('click', function() {
+    h1.innerHTML = "Badal gaya"
+})
+
+
+
+
+
+
+
+
+
