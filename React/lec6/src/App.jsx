8750e99c065adb235472b3487429fc0223e6314a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-28 23:16:51 +0530

    feat: update App component to include multiple Navbar instances with dynamic props

--- React/lec6/src/App.jsx
+++ React/lec6/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className="bg-blue-500 text-white p-4">App</div>
+    <div>
+      <Navbar title = 'Shreyesh' color = 'red'links ={[ 'Home', 'About', 'Contact' ]} />
+      <Navbar title = 'Ken' color = 'blue' links ={[ 'Home', 'About', 'Contact' ]} />
+      <Navbar title = 'kaneki' color = 'green' links ={[ 'Home', 'About', 'Contact' ]} />
+    </div>
   )
 }
 
