b875f92a82bd3f6114b5ec3af8d3f792c7b04445
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-28 11:31:56 +0530

    feat: update App component to include Navbar and Section for improved layout

--- class1-task/src/App.jsx
+++ class1-task/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Section from './components/Section'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Section />
+    </div>
   )
 }
+
+
 
 export default App
