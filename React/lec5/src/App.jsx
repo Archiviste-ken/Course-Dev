b24e1460d4bea1c3fbfc1bd9b7b1f04ce2dd1888
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-28 19:39:19 +0530

    refactor: simplify App component by removing unused variables and rendering logic

--- React/lec5/src/App.jsx
+++ React/lec5/src/App.jsx
@@ -4,21 +4,13 @@ import Card from './components/Card'
 
 const App = () => {
 
-  const age = 18
-  const name = 'Shreyesh'
-  const arr = [10,20,30]
+  
   return (
     
-    <div>
-      <div className = 'text-xl text-red-600'>App</div>
-      <h1 className = 'text-indigo-100'>Age : {age}</h1>
-      <h1 className = 'text-indigo-100'>Name : {name}</h1>
-      <h1 className = 'text-green-800'>Array : {arr.filter(function(elem){
-          return elem > 15
-      }).map(function(elem){  
-          return <h1>{elem/2}</h1>
-      })}</h1>
-      <Card />
+    <div className = 'p-3 h-screen bg-black'>
+      {Card('sarthak')}
+      {Card('harwhal')}
+      {Card('sarthak')}
     </div>
   )
 }
