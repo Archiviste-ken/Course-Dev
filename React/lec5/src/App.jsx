d3d4d8b4616c044cb31d61edbb333101912d7be5
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-28 17:33:50 +0530

    feat: restructure App component layout and enhance readability

--- React/lec5/src/App.jsx
+++ React/lec5/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Card from './components/Card'
+
 
 const App = () => {
+
+  const age = 18
+  const name = 'Shreyesh'
+  const arr = [10,20,30]
   return (
-    <div className = 'text-xl text-red-600'>App</div>
+    
+    <div>
+      <div className = 'text-xl text-red-600'>App</div>
+      <h1 className = 'text-indigo-100'>Age : {age}</h1>
+      <h1 className = 'text-indigo-100'>Name : {name}</h1>
+      <h1 className = 'text-indigo-100'>Array : {arr.map(function(elem){
+          return elem
+      })}</h1>
+      <Card />
+    </div>
   )
 }
 
