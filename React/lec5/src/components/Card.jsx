4c788d51d61c4636c88b6bfaab8d01f6c260e98a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-28 19:39:54 +0530

    refactor: update Card component to accept props and render dynamic content

--- React/lec5/src/components/Card.jsx
+++ React/lec5/src/components/Card.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className = 'text-xl text-cyan-600'>Card</div>
+    <div className = 'bg-white border-2 border-red-500 m-10 rounded px-10 py-3 w-fit'>
+        <h1 className = 'text-4xl font-semibold '>{props}</h1>
+       
+        </div>
   )
 }
 
