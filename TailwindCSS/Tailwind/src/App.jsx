556fb448542c6652b63be0289ba779ba0c420750
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 08:06:46 +0530

    feat: add initial setup for TailwindCSS and React application
    - Added SVG assets for React and Vite.
    - Created Navbar component with basic styling.
    - Set up TailwindCSS in index.css.
    - Configured main entry point for React application.
    - Established Vite configuration with React and TailwindCSS plugins.

---
+++ TailwindCSS/Tailwind/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className = "text-3xl font-bold text-green-800"> Suno, mere veere</h1>
+      <h2 className = "text-2xl font-semibold text-blue-600"> Kisi se pyaar hai</h2>
+
+      <Navbar />
+    </div>
+  )
+}
+
+export default App 
