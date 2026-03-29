563f8ece017d604142a9dd82a6a3fe2bf6cd3482
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 07:42:56 +0530

    feat: add Men component to display voter ID information

---
+++ React/lec6/src/components/Men.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Men = () => {
+  return (
+    <div className="px-5 py-2 w-fit m-2 bg-blue-600 rounded " >
+        <h1 className='text-white text-lg font-bold'>voter id</h1>
+    </div>
+  )
+}
+
+export default Men
