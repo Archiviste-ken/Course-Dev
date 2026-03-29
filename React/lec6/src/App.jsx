390fbbe37385f6d7b6e8f14c59d2aa6ed50179cc
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 07:42:46 +0530

    refactor: comment out Navbar instances and update rendering logic based on age

--- React/lec6/src/App.jsx
+++ React/lec6/src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
+import Men from './components/Men'
 
 const App = () => {
+  const age = 12
+
+
   return (
     <div>
-      <Navbar title = 'Shreyesh' color = 'red'links ={[ 'Home', 'About', 'Contact' ]} />
+      {/* <Navbar title = 'Shreyesh' color = 'red'links ={[ 'Home', 'About', 'Contact' ]} />
+      <Men />
       <Navbar title = 'Ken' color = 'blue' links ={[ 'Home', 'About', 'Contact' ]} />
       <Navbar title = 'kaneki' color = 'green' links ={[ 'Home', 'About', 'Contact' ]} />
+      <Navbar title = 'Apple' color ='black' links ={['IPhone', 'Mac', 'Ipad', 'Watch']} /> */}
+  {age >= 10? <Men /> : <Women />}
+
     </div>
   )
-}
-
+} 
 export default App
+
+
+
