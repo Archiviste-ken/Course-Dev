41f83e9ace0b8d09ab8b0702a9950f24a56554f5
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 14:01:17 +0530

    feat: refactor App component to remove unused state and streamline form submission

--- React/lec8/src/App.jsx
+++ React/lec8/src/App.jsx
@@ -1,11 +1,21 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 const App = () => {
 
-  const [num, setNum] = useState(0)
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+    
+  }
   return (
-    <div className ='bg-black min-h-screen text-white'>
+    <div className = 'bg-black h-screen '>
+      <form className = 'flex gap-4 p-20' onSubmit = {submitHandler}>
+        <input className ='bg-gray-500 text-white gap-5 ' type="text" placeholder="Enter text here"/>
 
+        <button className = 'bg-gray-500 text-white rounded-xs'>Submit</button>
+        
+      </form>
+      
     </div>
   )
 }
