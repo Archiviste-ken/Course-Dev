039d840cec692f149f51934a89d1062fd5ef3474
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 22:41:01 +0530

    feat: implement form handling with state management in App component

--- React/lec9/src/App.jsx
+++ React/lec9/src/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => {
+
+  const [username, setUserName] = useState("")// 2 way binding
+  const [userRole, setUserRole] = useState('')
+  const [image, setimage] = useState('')
+  const [description, setDescription] = useState('')
+
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(username);
+
+  }
+  
   return (
     <div className=' h-screen bg-black text-white '>
 
-      <form className = 'p-2 flex flex-wrap justify-center '>
-        <input className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Enter your name'/>
+      <form className = 'p-2 flex flex-wrap justify-center ' onSubmit={submitHandler}>
+
+        <input onChange = {(e) =>{
+          setUserName(e.target.value)
+        }} value={username} className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Enter your name'/>
+
         <input className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Profile URI'/>
+
         <input className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Enter Description'/>
+
         <input className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Enter your role'/>
+
         <button className= 'border-2 px-5 py-2 active:scale-95 cursor-pointer rounded m-2 w-[90%] bg-emerald-700'>Submit</button>
+
       </form>
       
     </div>
