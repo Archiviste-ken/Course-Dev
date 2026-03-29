6319a94ec93e9077b3a03295723eae0a795066c9
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 17:34:25 +0530

    feat: implement controlled input for name in App component

--- React/lec8/src/App.jsx
+++ React/lec8/src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
-
+  const [name, setName] = useState('')
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('form submitted');
+    console.log(name);
     
   }
   return (
     <div className = 'bg-black h-screen '>
+
       <form className = 'flex gap-4 p-20' onSubmit = {submitHandler}>
-        <input className ='bg-gray-500 text-white gap-5 ' type="text" placeholder="Enter text here"/>
+
+        <input className ='bg-gray-500 text-white gap-5 ' type="text" placeholder="Enter text here"
+        value = {name}
+        onChange={(e) =>{setName(e.target.value)}}
+        />
 
         <button className = 'bg-gray-500 text-white rounded-xs'>Submit</button>
         
@@ -21,3 +26,5 @@ const App = () => {
 }
 
 export default App
+
+   
