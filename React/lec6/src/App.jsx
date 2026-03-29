33ee761fd0548def8aa8467b117eb78c142b0549
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 07:52:48 +0530

    feat: replace age-based rendering with button click functionality in App component

--- React/lec6/src/App.jsx
+++ React/lec6/src/App.jsx
@@ -1,23 +1,43 @@
+// import React from 'react'
+// // import Navbar from './components/Navbar'
+// import Men from './components/Men'
+
+// const App = () => {
+//   const age = 12
+
+
+//   return (
+//     <div>
+//       {/* <Navbar title = 'Shreyesh' color = 'red'links ={[ 'Home', 'About', 'Contact' ]} />
+//       <Men />
+//       <Navbar title = 'Ken' color = 'blue' links ={[ 'Home', 'About', 'Contact' ]} />
+//       <Navbar title = 'kaneki' color = 'green' links ={[ 'Home', 'About', 'Contact' ]} />
+//       <Navbar title = 'Apple' color ='black' links ={['IPhone', 'Mac', 'Ipad', 'Watch']} /> */}
+//   {age >= 10? <Men /> : <Women />}
+
+//     </div>
+//   )
+// } 
+// export default App
+
+
+
+
 import React from 'react'
-// import Navbar from './components/Navbar'
-import Men from './components/Men'
 
 const App = () => {
-  const age = 12
 
-
+  const btnClicked = () => {
+    console.log("button is clicked!!!");
+    
+  }
   return (
     <div>
-      {/* <Navbar title = 'Shreyesh' color = 'red'links ={[ 'Home', 'About', 'Contact' ]} />
-      <Men />
-      <Navbar title = 'Ken' color = 'blue' links ={[ 'Home', 'About', 'Contact' ]} />
-      <Navbar title = 'kaneki' color = 'green' links ={[ 'Home', 'About', 'Contact' ]} />
-      <Navbar title = 'Apple' color ='black' links ={['IPhone', 'Mac', 'Ipad', 'Watch']} /> */}
-  {age >= 10? <Men /> : <Women />}
-
+      <button onClick={ btnClicked} className =  'active:scale-95 bg-emerald-600 text-white px-6 py-4 rounded font-bold m-2'> Click me to download</button>
     </div>
   )
-} 
+}
+
 export default App
 
 
