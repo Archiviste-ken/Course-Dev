42d770d924ac9d5c1a6b6995bae833ac8adf21eb
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 18:27:57 +0530

    feat: update state management in App component to use title instead of name

--- React/lec8/src/App.jsx
+++ React/lec8/src/App.jsx
@@ -1,10 +1,21 @@
 import React, {useState} from 'react'
 
 const App = () => {
-  const [name, setName] = useState('')
+  const [title, setTitle] = useState('')
+  
+  const [allUsers, setAllUsers] = useState(['harsh'])
+
+
   const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(name);
+    e.preventDefault()
+   
+    const newAllUsers =  [...allUsers]
+    newAllUsers.push(title)
+    console.log(newAllUsers)
+
+    setAllUsers(newAllUsers)
+    
+    setTitle('')
     
   }
   return (
@@ -13,14 +24,20 @@ const App = () => {
       <form className = 'flex gap-4 p-20' onSubmit = {submitHandler}>
 
         <input className ='bg-gray-500 text-white gap-5 ' type="text" placeholder="Enter text here"
-        value = {name}
-        onChange={(e) =>{setName(e.target.value)}}
+        value = {title}
+        required
+        onChange={(e) =>{setTitle(e.target.value)}}
         />
 
         <button className = 'bg-gray-500 text-white rounded-xs'>Submit</button>
         
       </form>
       
+
+      {allUsers.map(function(elem){
+        return <h4 className='text-white'>{elem}</h4>
+      })}
+    
     </div>
   )
 }
