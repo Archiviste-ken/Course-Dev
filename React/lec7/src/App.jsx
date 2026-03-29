4640a51bfb5d1a1efd511d931699157bbf9d534f
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 12:37:40 +0530

    feat: initialize React app with Vite, add main components and styles
    - Created App component with counter functionality and layout.
    - Added CSS styles for various components including hero section and buttons.
    - Included SVG assets for React and Vite logos.
    - Set up index.css for global styles and theming.
    - Configured Vite for React application.

--- React/lec7/src/App.jsx
+++ React/lec7/src/App.jsx
@@ -1,39 +1,67 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const App = () => {
-  const [num, setNum] = useState(10);
+// const App = () => {
+//   const [num, setNum] = useState(10);
 
  
 
   
+//   return (
+//     <div>
+//       <h1 className="bg-gray-300 w-10 mx-auto text-center m-1 font-bold rounded border-2 border-white">
+//         {num}
+//       </h1>
+//       <div className="flex justify-center">
+//         <button
+//           className=" bg-gray-400 w-fit m-1 font-bold px-2 py-2 rounded border-2 border-white"
+//           onClick= {() => {
+//     console.log("btn clicked");
+//     setNum(num + 1);
+//   }}
+//         >
+//           Increement
+//         </button>
+//         <button
+//           className="bg-gray-400  m-1 font-bold px-2 py-2 rounded border-2 border-white"
+//           onClick = {() => {
+//     console.log("btn again clicked");
+//     setNum(num - 1);
+//   }}    
+//         >
+//           {" "}
+//           Decreement
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+import React, { useState } from 'react'
+
+
+
+const App = () => {
+
+  const [num, setNum] = useState(0)
+
   return (
     <div>
-      <h1 className="bg-gray-300 w-10 mx-auto text-center m-1 font-bold rounded border-2 border-white">
+      <div className="bg-gray-300 w-10 mx-auto text-center m-1 font-bold rounded border-2 border-white">
         {num}
-      </h1>
-      <div className="flex justify-center">
-        <button
-          className=" bg-gray-400 w-fit m-1 font-bold px-2 py-2 rounded border-2 border-white"
-          onClick= {() => {
-    console.log("btn clicked");
-    setNum(num + 1);
-  }}
-        >
-          Increement
-        </button>
-        <button
-          className="bg-gray-400  m-1 font-bold px-2 py-2 rounded border-2 border-white"
-          onClick = {() => {
-    console.log("btn again clicked");
-    setNum(num - 1);
-  }}
-        >
-          {" "}
-          Decreement
-        </button>
+      </div>
+
+      <div className="flex justify-center"> <button className="bg-gray-400 w-fit m-1 font-bold px-2 py-2 rounded border-2 border-white" onClick ={() => {
+        const rdm = Math.floor (Math.random()*100)
+        setNum(rdm)
+
+        }}>ClickMe </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
