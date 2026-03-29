9175b7ae9ccbbe92322f7498fedc5bf2c662d7d7
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-29 19:49:26 +0530

    feat: update App component to include a form for user input

--- React/lec9/src/App.jsx
+++ React/lec9/src/App.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, React!</h1>
+    <div className=' h-screen bg-black text-white '>
+
+      <form className = 'p-2 flex flex-wrap justify-center '>
+        <input className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Enter your name'/>
+        <input className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Profile URI'/>
+        <input className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Enter Description'/>
+        <input className =" border-2 px-5 py-2 rounded m-2 w-[45%]"type= "text" placeholder='Enter your role'/>
+        <button className= 'border-2 px-5 py-2 active:scale-95 cursor-pointer rounded m-2 w-[90%] bg-emerald-700'>Submit</button>
+      </form>
+      
     </div>
   )
 }
