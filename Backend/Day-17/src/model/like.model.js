3ab07d0ea02c251772eea7b66761d9dadaf69bd2
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-30 17:52:15 +0530

    feat: add Like model with schema and unique index

---
+++ Backend/Day-17/src/model/like.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+
+const LikeSchema = new mongoose.Schema({
+
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "posts",
+        required: [ true, "Post is required"]   
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: [ true, "User is required"]   
+    },
+
+}, {
+    timestamps: true
+})
+
+LikeSchema.index({ post: 1, user: 1 }, { unique: true })
