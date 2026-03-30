77d3279e092b1888cf12963bf296f6c9e6d60980
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-30 07:08:36 +0530

    refactor: rearrange middleware usage for improved clarity in app.js

--- Backend/Day-17/src/app.js
+++ Backend/Day-17/src/app.js
@@ -3,14 +3,22 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser")
 
 const app = express()
-
-const postRouter = require("./routes/post.routes")
-const authRouter = require("./routes/auth.routes")
-
 app.use(express.json())
 app.use(cookieParser()); // ✅ MOVE THIS UP
 
+
+/* require routes */
+const postRouter = require("./routes/post.routes")
+const authRouter = require("./routes/auth.routes")
+const userRouter = require("./routes/user.routes")
+
+
+
+/* using routes */
 app.use("/api/auth", authRouter)
 app.use("/api/posts", postRouter)
+app.use("/api/users", userRouter)
+
+
 
 module.exports = app
