20d9de0ae346101c9d020533ef9052baf642894f
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-30 08:20:12 +0530

    fix: correct function parameters order in followUserController and update followModel import path

--- Backend/Day-17/src/controllers/user.controller.js
+++ Backend/Day-17/src/controllers/user.controller.js
@@ -1,9 +1,21 @@
-const followModel = require("../models/follow.model")
+const followModel = require("../model/follow.model")
 
 
-async function followUserContoller(res,req){
+async function followUserContoller(req,res){
 
 
+    const followerUsername = req.user.username
+    console.log(req.user);
+    const followeeUsername = req.params.username
+    
+    const followRecord = await followModel.create({
+        follower: followerUsername,
+        followee: followeeUsername
+    })
+    res.status(201).json({
+        message: `You are now following ${followeeUsername}`,
+        data: followRecord
+    })
 }
     module.exports = {
     followUserContoller
