f60e8afaa2bf00493d8b93886174ca006db23a43
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-31 13:07:26 +0530

    fix: standardize naming conventions in like model and improve error messages

--- Backend/Day-17/src/model/like.model.js
+++ Backend/Day-17/src/model/like.model.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
+const { post } = require("../routes/post.routes");
 
-
-const LikeSchema = new mongoose.Schema({
-
+const likeSchema = new mongoose.Schema({
     post: {
+       
         type: mongoose.Schema.Types.ObjectId,
         ref: "posts",
-        required: [ true, "Post is required"]   
+        required: [ true, "post id is required for creating a like"]
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-        required: [ true, "User is required"]   
-    },
+       
+        type: String,
+        ref: "users",  
+        required: [true, "username is required for creating a like"]
+    }
+}, { timestamps: true }
+)
 
-}, {
-    timestamps: true
-})
 
-LikeSchema.index({ post: 1, user: 1 }, { unique: true })
+likeSchema.index({ post: 1, user: 1 }, { unique: true }) // This creates a compound index on the "post" and "user" fields. The unique: true option ensures that each combination of post and user can only occur once in the collection. This means that a user can only like a specific post once, preventing duplicate likes from the same user on the same post.
 
-const LikeModel = mongoose.model("likes", LikeSchema)
+const likeModel = mongoose.model("likes", likeSchema)
 
-module.exports = LikeModel
+module.exports = likeModel
