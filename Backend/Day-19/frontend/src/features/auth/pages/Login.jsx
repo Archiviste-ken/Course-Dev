9daf34d216a41098dd2f3d48ed13407685e8fd36
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-03 18:55:32 +0530

    feat: add styling for login page and update structure to use <main> tag

--- Backend/Day-19/frontend/src/features/auth/pages/Login.jsx
+++ Backend/Day-19/frontend/src/features/auth/pages/Login.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import'../style/form.scss'
 
 const Login = () => {
   return (
-    <div className = "form-container">
+    <main className = "form-container">
       <h1>Login</h1>
       <form>
         <input type="text" name = "username"placeholder='enter username' />
         <input type="password" name = "password" placeholder='enter password' />
         <button type='submit'>Login</button>
       </form>
-    </div>
+    </main>
   )
 }
 
