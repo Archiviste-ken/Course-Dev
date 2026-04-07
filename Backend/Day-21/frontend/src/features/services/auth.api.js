b43747fc166a6a04a722bd3d335d16af8560757c
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-07 11:15:31 +0530

    feat: configure axios instance in auth.api.js with baseURL and implement login, register, and getMe functions

--- Backend/Day-21/frontend/src/features/services/auth.api.js
+++ Backend/Day-21/frontend/src/features/services/auth.api.js
@@ -2,5 +2,35 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL
+    baseURL: "http://localhost:3000/api/login",
+    withCredentials: true,
 })
+
+export async function login(username,password){
+
+    const response = await  api.post("./login",{
+
+        username, password
+    })
+
+    return response.data
+}
+
+
+export async function register(username, email, password){
+
+    const response = await api.post('./register',{
+
+        username,email, password
+    })
+
+
+    return response.data
+}
+
+export async function getMe(){
+    const response = await api.get('/get-me')
+
+
+    return response.data
+}
