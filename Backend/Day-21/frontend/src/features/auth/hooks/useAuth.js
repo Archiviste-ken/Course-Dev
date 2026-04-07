c6079380170b712b83e21c53b19dc6b233b676af
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-07 20:03:20 +0530

    fix: remove redundant import statement for getMe in useAuth hook

--- Backend/Day-21/frontend/src/features/auth/hooks/useAuth.js
+++ Backend/Day-21/frontend/src/features/auth/hooks/useAuth.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../auth.context";
 import { login } from "../../services/auth.api";
 import { register } from "../../services/auth.api";
-import { getMe } from "../../services/auth.api";
+import { getMe } from "../../services/auth.api"; 
 
 export const useAuth = () => {
 
