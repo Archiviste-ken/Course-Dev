b3b0bb724cf77ced5e75a4a7180cb21154bc2fe1
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-07 18:48:12 +0530

    feat: implement useAuth hook for authentication context management

---
+++ Backend/Day-21/frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { AuthContext } from "../auth.context";
+import { login } from "../../services/auth.api";
+import { register } from "../../services/auth.api";
+import { getMe } from "../../services/auth.api";
+
+export const useAuth = () => {
+
+    const context = useContext(AuthContext);
+    
+    const { user, setUser, loading, setLoading } = context;
+
+    const handleLogin = async (username, password) => {
+
+        setLoading(true);
+
+        const response = await login(username, password);
+
+}
+
+}
