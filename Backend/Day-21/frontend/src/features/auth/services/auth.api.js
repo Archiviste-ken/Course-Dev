f67ac0f42577828ec0d0a5f2836577cc6b517a51
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-15 20:06:29 +0530

    feat: implement authentication API layer with login, register, and getMe functions

---
+++ Backend/Day-21/frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,51 @@
+            // API LAYER FOR AUTHENTICATION 
+
+
+
+
+// src/services/auth.api.js
+
+import axios from "axios";
+
+const api = axios.create({  // axios.create() is used to create your own custom Axios instance.
+  baseURL: "http://localhost:3000/api/auth", 
+  withCredentials: true,  // send cookies with requests everytime.
+});
+
+export async function login(username, password) {
+  try {
+    const response = await api.post("/login", {
+      username,
+      password,
+    });
+    
+    return response.data;
+  } catch (error) {
+    console.log("Login error:", error.response?.data || error.message);
+    throw error;
+  }
+}
+
+export async function register(username, email, password) {
+  try {
+    const response = await api.post("/register", {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.log("Register error:", error.response?.data || error.message);
+    throw error;
+  }
+}
+
+export async function getMe() {
+  try {
+    const response = await api.get("/get-me");
+    return response.data;
+  } catch (error) {
+    console.log("GetMe error:", error.response?.data || error.message);
+    throw error;
+  }
+}
