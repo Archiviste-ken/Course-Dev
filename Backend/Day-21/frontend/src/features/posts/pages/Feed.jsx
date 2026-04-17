7f322de5f198489e23857998c6c3cf790ba4e7fd
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-17 15:40:31 +0530

    style: update form styles and add background color; fix newline in usePost hook

--- Backend/Day-21/frontend/src/features/posts/pages/Feed.jsx
+++ Backend/Day-21/frontend/src/features/posts/pages/Feed.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import "../style/feed.scss"
 import Post from '../components/Post'
 import { usePost } from '../hook/usePost'
-// import Nav from '../../shared/components/Nav'
+// import Nav from '../../shared/components/Nav' 
 
 const Feed = () => {
 
