a3f8c9078dbe861251eb5c81a1a903ec94933d14
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-19 22:30:45 +0530

    feat: add CreatePost component and navigation; enhance Feed layout and styles

--- Backend/Day-21/frontend/src/features/posts/pages/feed.jsx
+++ Backend/Day-21/frontend/src/features/posts/pages/feed.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import "../style/feed.scss";
 import Post from "../components/Post";
 import { usePost } from "../hook/usePost";
+import Nav from "../../shared/components/nav";
 
 const Feed = () => {
   const { feed, handleGetFeed, loading } = usePost();
@@ -23,6 +24,7 @@ const Feed = () => {
 
   return (
     <main className="feed-page">
+      <Nav />
       <div className="feed">
         <div className="posts">
           {feed.map((post, index) => {
---
+++ Backend/Day-21/frontend/src/features/shared/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import  "../nav.scss";
+import { useNavigate } from 'react-router';
+
+const Nav = () => {
+  const navigate = useNavigate();
+  return (
+        <nav className='nav-bar'>
+
+            <p>Insta</p>
+            <button className='button primary-button' onClick={() => navigate('/create-post')}> new post </button>
+
+
+        </nav>
+  )
+}
+
+export default Nav
