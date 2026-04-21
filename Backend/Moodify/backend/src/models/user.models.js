63d88f4c5ef2c5998a6ac1ea9e4dc87aee144199
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-22 01:44:09 +0530

    feat: create user model with schema for username, password, and email

---
+++ Backend/Moodify/backend/src/models/user.models.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    
+    username:{
+        type: String,
+        require: [true, "Username is required"],
+        unique: [ true, "Username must be unique"]
+    },
+
+    password:{
+        type: String,
+        require: [true, "password is required"],
+        unique: [true, "password must be unique"],
+    },
+
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "Email must be unique"],
+    }
+
+})
+
+
+const userModel = mongoose.model("users",   userSchema)
+
+module.exports = userModel;
