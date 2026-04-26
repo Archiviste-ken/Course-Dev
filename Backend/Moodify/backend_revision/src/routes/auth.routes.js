d371520d7f6bc6186c78026ec77558c1d844e038
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-26 15:36:28 +0530

    feat: add route for retrieving current user information

--- Backend/Moodify/backend_revision/src/routes/auth.routes.js
+++ Backend/Moodify/backend_revision/src/routes/auth.routes.js
@@ -12,5 +12,8 @@ router.post("/register",authController.userRegister)
 router.post("/login",authController.userLogin)
 
 
+router.post("/get-me",authController.getUser)
+
+
 
 module.exports = router
