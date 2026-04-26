d0b532fc6d1e309cae4359e466d9d0da46b29d6d
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-26 15:36:20 +0530

    feat: implement authUser middleware for token verification

---
+++ Backend/Moodify/backend_revision/src/Middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+const userModel = require("../models/user.model");
+
+const jwt = require("jsonwebtoken")
+
+async function authUser(req,res,next){
+
+
+const token = req.cookies.token;
+
+
+if(!token){
+
+    return res.status(401).json({
+
+    message: "Token not provided"
+
+    })
+
+    try {
+
+
+    const decoded = jwt.verify(
+        token,
+    process.env.JWT_SECRET_KEY,
+    )
+    
+    }
+
+    
+
+
+
+}
