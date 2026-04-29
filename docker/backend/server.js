a7ce454627549a50c74a70ab03d3c9c3db259ebb
Shreyesh <shreyeshpachpor06@gmail.com>
2026-04-29 20:16:30 +0530

    feat: update package.json to use module type and refactor server.js to use ES modules

--- docker/backend/server.js
+++ docker/backend/server.js
@@ -1,7 +1,21 @@
-const app = require("./src/app")
+import express from 'express';
+
+const app = express();
 
 
-app.listen(3000,(req,res)=>{
-    console.log("Server is now running on the port 3000");
-    
-})
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello from the backend server!' });
+});
+
+    app.get('/api/data', (req, res) => {
+    const data = {
+        id: 1,
+        name: 'Sample Data',
+        description: 'This is some sample data from the backend server.'
+    };
+    res.status(200).json(data);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
