b3f386223989bc38d2cf42f505e617dd9cf62fa2
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-02 00:21:51 +0530

    refactor: clean up server.js by removing redundant line breaks and improving formatting

--- Backend/Moodify/backend/server.js
+++ Backend/Moodify/backend/server.js
@@ -1,14 +1,9 @@
-require('dotenv').config()
+require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
+connectDB();
 
-
-connectDB()
-
-
-app.listen(3000,(req,res)=> {
-    console.log("Sever is running on the port 3000");
-    
-})
-
+app.listen(3000, (req, res) => {
+  console.log("Sever is running on the port 3000");
+});
