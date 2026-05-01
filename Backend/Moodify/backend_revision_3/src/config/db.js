36730fe0873645f6e3ce4ba6b29357295cfd2dfc
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-02 00:23:40 +0530

    feat: add db.js to establish MongoDB connection using Mongoose

---
+++ Backend/Moodify/backend_revision_3/src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+
+async function connectDB(){
+
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+
+        console.log("Connected to the MongoDB");
+        
+    })
+
+}
+
+
+module.exports = connectDB
