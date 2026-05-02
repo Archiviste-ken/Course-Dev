93ff0462cff2c3543d44dc3f4cb2dec60a636b2b
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-02 15:40:29 +0530

    feat: add authentication middleware to getUser route for user identification

--- Backend/Moodify/backend_revision_3/src/routes/auth.routes.js
+++ Backend/Moodify/backend_revision_3/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const {Router} = require("express")
 const authController = require("../controllers/auth.controllers")
+const authMiddleware = require("../middlewares/auth.middleware")
 const router = Router()
 
 
@@ -7,7 +8,7 @@ router.post("/register",authController.userRegister)
 
 router.post("/login",authController.userLogin)
 
-router.get("/get-me",authController.getUser)
+router.get("/get-me",authMiddleware.identifyUser, authController.getUser)
 
 
 
