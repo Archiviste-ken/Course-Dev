c5e79929e361902f45d4432cc8130cd076df6e4d
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-03 12:53:13 +0530

    style: improve formatting and add comments in app.routes.jsx for clarity

--- Backend/Moodify/frontend/src/app.routes.jsx
+++ Backend/Moodify/frontend/src/app.routes.jsx
@@ -1,26 +1,19 @@
-import { createBrowserRouter } from "react-router"
-import Login from "./features/auth/pages/Login"
-import Register from "./features/auth/pages/Register" 
+import { createBrowserRouter } from "react-router"; // creates a router config object that tells you app which url shows which component.
+import Login from "./features/auth/pages/Login";
+import Register from "./features/auth/pages/Register";
 
+export const router = createBrowserRouter([ // router is a variable which stores/holds the routes
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
 
-
-export const router = createBrowserRouter([
-
-    {
-        path: "/",
-        element: <h1>Home</h1>
-    },
-
-    {
-        path: "/register",
-        element: <Register />
-    },
-    {
-        path: "/login",
-        element: <Login />
-    }
-    
-
-])
-
-
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
