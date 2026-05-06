6a8a2eb3aaefa6f79979acd30878447dd02c0f1a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-06 13:33:07 +0530

    feat: implement Register component with FormGroup for input fields

--- Backend/Moodify/frontend/src/features/auth/pages/Register.jsx
+++ Backend/Moodify/frontend/src/features/auth/pages/Register.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import "../style/register.scss";
+import React from "react";
+import FormGroup from "../components/FormGroup";
+import { Link } from "react-router";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <main className="register-page">
+      <div className="form-container">
+        <h1>Register</h1>
+        <form>
+          <FormGroup label="Username" placeholder="Enter your username" />
+          <FormGroup label="Email" placeholder="Enter your email" />
+          <FormGroup label="Password" placeholder="Enter your password" />
+          <button className="button" type="submit">Register</button>
+          <p>Already have an account? <Link to="/login">Login here</Link></p>
+        </form>
+      </div>
+    </main>
   )
 }
 
