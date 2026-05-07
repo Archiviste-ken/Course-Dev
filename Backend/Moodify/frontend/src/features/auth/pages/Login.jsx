b02d76df1706afad5fd4a6684754bfc2f3a68e3b
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-07 23:35:40 +0530

    feat: implement login form with state management and submission handling

--- Backend/Moodify/frontend/src/features/auth/pages/Login.jsx
+++ Backend/Moodify/frontend/src/features/auth/pages/Login.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/login.scss";
 import FormGroup from "../components/FormGroup";
 import { Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 
 const Login = () => {
+  const { loading, handleLogin } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <main className="login-page">
       <div className="form-container">
         <h1>Login</h1>
-        <form>
-          <FormGroup label="Email" placeholder="Enter your email" />
-          <FormGroup label="Password" placeholder="Enter your password" />
-          <button className="button" type="submit">Login</button>
-          <p>Don't have an account? <Link to="/register">Register here</Link></p>
+        <form onSubmit={handleSubmit}>
+          <FormGroup
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email"
+            placeholder="Enter your email"
+          />
+          <FormGroup
+          value= {password}
+          onChange= {(e) => setPassword(e.target.value)}
+          label="Password" placeholder="Enter your password" />
+          <button className="button" type="submit">
+            Login
+          </button>
+          <p>
+            Don't have an account? <Link to="/register">Register here</Link>
+          </p>
         </form>
       </div>
     </main>
   );
 };
 
-export default Login; 
-        
-
-
+export default Login;
