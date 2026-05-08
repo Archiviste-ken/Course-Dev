011f77c529ebcfcc24b34a4724398f19c21ae69d
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-09 00:25:49 +0530

    feat: add node-id3 dependency and implement song metadata extraction in uploadSong function

--- Backend/Moodify/backend/src/controllers/song.controller.js
+++ Backend/Moodify/backend/src/controllers/song.controller.js
@@ -1,8 +1,13 @@
 const songModel = require("../models/song.model");
+const id3 = require("node-id3");
 
 async function uploadSong(req, res) {
-  console.log(req.file);
+
+    const songBuffer = req.file.buffer
+
+  const tags = id3.read(songBuffer);
+
+  console.log(tags);
 }
 
-
-module.exports = { uploadSong }
+module.exports = { uploadSong };
