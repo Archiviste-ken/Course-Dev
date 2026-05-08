3239c2ccb55d898a8e24094570686fa43217f0ee
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-08 13:22:45 +0530

    fix: refactor handleLogin function and ensure handleGetMe is called on mount

--- Backend/Moodify/frontend/src/features/auth/hooks/useAuth.js
+++ Backend/Moodify/frontend/src/features/auth/hooks/useAuth.js
@@ -2,6 +2,7 @@ import { login, register, getMe, logout } from "../services/auth.api";
 
 import { useContext } from "react";
 import { AuthContext } from "../auth.context";
+import { useEffect } from "react";
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
@@ -15,11 +16,30 @@ export const useAuth = () => {
     setLoading(false);
   }
 
-  async function handleLogin({ username, email, password }) {
-    setLoading(true);
-    const data = await login({ username, email, password });
-    setUser(data.user);
-    setLoading(false);
+  async function handleLogin({ email, password }) {
+    try {
+      setLoading(true);
+
+      const data = await login({
+        email,
+        password,
+      });
+
+      setUser(data.user);
+
+      return {
+        success: true,
+      };
+    } catch (error) {
+      console.log(error.response?.data);
+
+      return {
+        success: false,
+        message: error.response?.data?.message || "Login failed",
+      };
+    } finally {
+      setLoading(false);
+    }
   }
 
   async function handleGetMe() {
@@ -35,6 +55,10 @@ export const useAuth = () => {
     setUser(null);
     setLoading(false);
   }
+
+  useEffect(() => {
+    handleGetMe();
+  }, []);
 
   return {
     user,
