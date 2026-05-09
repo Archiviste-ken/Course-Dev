e33f8b0764a48a84947d72ca5b90086a6af53b8f
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-10 03:36:49 +0530

    feat: refactor server setup to use http server with Socket.io integration

--- Backend/Day-31/server.js
+++ Backend/Day-31/server.js
@@ -1,7 +1,18 @@
+import { log } from "console";
 import app from "./src/app.js";
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-app.listen(3000, () => {
-  console.log("server is now connected to the port 3000");
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(3000, ()=> {
+    console.log("Server is running on the port 3000");
+    
 });
