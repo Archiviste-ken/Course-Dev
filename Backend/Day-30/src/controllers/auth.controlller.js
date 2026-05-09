2e9797a9c2f2a8ba42ec222c4d0defadf4946856
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-09 22:28:41 +0530

    feat: update registerUser function to handle user existence error with appropriate status

--- Backend/Day-30/src/controllers/auth.controlller.js
+++ Backend/Day-30/src/controllers/auth.controlller.js
@@ -1,7 +1,16 @@
+// export async function registerUser(req, res, next) {
+//   try{
+//   throw new Error("encounter an error while registering new user");
+//   } catch(err){
+//     next(err)
+//   }
+// }
+
 export async function registerUser(req, res, next) {
-  try{
-  throw new Error("encounter an error while registering new user.");
-  } catch(err){
-    next(err)
+  try {
+    throw new Error("User already exists, with same email.");
+  } catch (err) {
+    err.status = 409;
+    next(err);
   }
 }
