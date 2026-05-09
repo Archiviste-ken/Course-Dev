ec64132dedcd0e0b3318de191ebc4818ea25ecb0
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-10 04:01:41 +0530

    refactor: clean up import statements and formatting in server.js

--- Backend/Day-31/server.js
+++ Backend/Day-31/server.js
@@ -1,18 +1,16 @@
-import { log } from "console";
 import app from "./src/app.js";
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
-
-
+const io = new Server(httpServer, {
+  /* options */
+});
 
 io.on("connection", (socket) => {
   // ...
 });
 
-httpServer.listen(3000, ()=> {
-    console.log("Server is running on the port 3000");
-    
+httpServer.listen(3000, () => {
+  console.log("Server is running on the port 3000");
 });
