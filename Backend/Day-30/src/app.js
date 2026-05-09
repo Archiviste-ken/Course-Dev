687949c52aaeab50036d8db9d97506e642ba06ff
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-09 22:28:27 +0530

    refactor: clean up app.js by removing unnecessary comments and fixing handleError middleware usage

--- Backend/Day-30/src/app.js
+++ Backend/Day-30/src/app.js
@@ -1,21 +1,13 @@
 import express from "express";
 import authRouter from "../src/routes/auth.routes.js";
-import handleError from "./middleware/error.middleware.js"
+import handleError from "./middleware/error.middleware.js";
 
 const app = express();
 
-
-/**
- * Middlewares
- */
-
 app.use(express.json());
-app.use(handleError())
-
-/**
- * Routes
- */
 
 app.use("/api/auth", authRouter);
+
+app.use(handleError);
 
 export default app;
