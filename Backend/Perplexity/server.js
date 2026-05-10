23e14b6bbcee5392676de8bdade660e0a90037dd
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-10 17:27:38 +0530

    feat: add user, chat, and message models with bcrypt password hashing

--- Backend/Perplexity/server.js
+++ Backend/Perplexity/server.js
@@ -2,11 +2,10 @@ import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
-connectDB().catch((err => {
-  console.error("MongoDB connection failed:", err)
-  process.exit(1)
-  
-}));
+connectDB().catch((err) => {
+  console.error("MongoDB connection failed:", err);
+  process.exit(1);
+});
 
 app.listen(3000, () => {
   console.log("server is now running on the port 3000");
