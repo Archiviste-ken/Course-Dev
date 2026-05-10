66b6d885b0c0155c8485ea0e72cef63e0f4cc5d4
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-10 14:40:26 +0530

    feat: enhance MongoDB connection handling and add cookie parser middleware

--- Backend/Perplexity/server.js
+++ Backend/Perplexity/server.js
@@ -2,7 +2,11 @@ import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
-connectDB();
+connectDB().catch((err => {
+  console.error("MongoDB connection failed:", err)
+  process.exit(1)
+  
+}));
 
 app.listen(3000, () => {
   console.log("server is now running on the port 3000");
