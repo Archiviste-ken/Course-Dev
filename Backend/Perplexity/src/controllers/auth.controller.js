840505cac056d394ba00f939300bbaf5b2a52aed
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-10 23:58:16 +0530

    fix: update user existence check response and improve error handling in registerUser function

--- Backend/Perplexity/src/controllers/auth.controller.js
+++ Backend/Perplexity/src/controllers/auth.controller.js
@@ -7,15 +7,39 @@ export async function registerUser(req, res) {
     $or: [email, username],
   });
 
-  if(isUserAlreadyExists){
-    return res.status(401).json({
-        message: "User already Exists, login in."
-    })
+  if (isUserAlreadyExists) {
+    return res.status(400).json({
+      message: "User already Exists, login in.",
+      success: false,
+      err: "User already exists",
+    });
   }
 
-  
+  const user = await userModel.create({
+    username,
+    password,
+    email,
+  });
 
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "3d",
+    },
+  );
 
+  res.cookie("cookie", cookie);
 
-
+  return res.status(201).json({
+    message: "The User is registered successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
 }
