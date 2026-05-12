1745b2688013f3f381b26185d7608b32751ea012
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-13 01:32:57 +0530

    fix: add login route to authentication router with validation

--- Backend/Perplexity/src/routes/auth.routes.js
+++ Backend/Perplexity/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { registerValidator } from "../validators/auth.validator.js";
-import { registerUser, verifyEmail } from "../controllers/auth.controller.js";
+import { registerValidator, loginValidator } from "../validators/auth.validator.js";
+import { registerUser, verifyEmail, loginUser } from "../controllers/auth.controller.js";
 
 const authrouter = Router();
 
 authrouter.post("/register", registerValidator, registerUser);
 
 authrouter.get("/verify-email", verifyEmail);
+
+authrouter.post("/login", loginValidator, loginUser);
 
 export default authrouter;
