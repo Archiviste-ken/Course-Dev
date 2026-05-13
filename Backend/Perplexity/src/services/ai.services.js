aff89c7e42acc5699c23c4138a5661503be2323c
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-13 19:45:20 +0530

    fix: update ai.services.js to use environment variable for API key

--- Backend/Perplexity/src/services/ai.services.js
+++ Backend/Perplexity/src/services/ai.services.js
@@ -2,5 +2,5 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
-  apiKey: "your-api-key"
+  apiKey: process.env.GEMINI_API_KEY
 });
