6c1ac1af499caaec9e1bc42a09b100e995407bad
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-13 20:02:29 +0530

    feat: import and invoke testAI function in server.js

--- Backend/Perplexity/server.js
+++ Backend/Perplexity/server.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
+import { testAI } from "./src/services/ai.services.js";
+
+testAI();
 
 connectDB().catch((err) => {
   console.error("MongoDB connection failed:", err);
