d9eae7fb8cbcbdd7f93d23276b7b30b490ea7593
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-13 23:02:29 +0530

    feat: update index.js to integrate ChatMistralAI and dotenv for environment variables

--- Backend/Gen-AI/index.js
+++ Backend/Gen-AI/index.js
@@ -1,11 +1,20 @@
+import "dotenv/config";
 import readline from "readline";
+import { ChatMistralAI } from "@langchain/mistralai";
 
-const rl = readline.createInterface({
+const rl = readline.createInterface({   // helps take input from the user and print output to the console.
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("What is you name? ", (name) => {
-  console.log(`Hello ${name}!`);
-  rl.close();
+const model = new ChatMistralAI({
+  model: "mistral-small-latest",
+  temperature: 0,
+  apiKey: process.env.MISTRAL_API_KEY,
 });
+
+const resonse = await model.invoke("What is the capital of Russia?");
+
+console.log(resonse.text);
+
+rl.close();
