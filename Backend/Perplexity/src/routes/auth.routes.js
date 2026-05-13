7045772d2f1763dfe8703f3095d82243bcc33edc
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-13 15:08:51 +0530

    fix: improve code formatting and add missing documentation for routes

--- Backend/Perplexity/src/routes/auth.routes.js
+++ Backend/Perplexity/src/routes/auth.routes.js
@@ -1,13 +1,47 @@
 import { Router } from "express";
-import { registerValidator, loginValidator } from "../validators/auth.validator.js";
-import { registerUser, verifyEmail, loginUser } from "../controllers/auth.controller.js";
+import {
+  registerValidator,
+  loginValidator,
+} from "../validators/auth.validator.js";
+import {
+  registerUser,
+  verifyEmail,
+  loginUser,
+} from "../controllers/auth.controller.js";
 
 const authrouter = Router();
 
+/**
+ *  @route POST /api/auth/register
+ *  @desc Register a new user
+ *  @access Public
+ * @body { username, email, password }
+ */
+
 authrouter.post("/register", registerValidator, registerUser);
 
+/**
+ * @route GET /api/auth/verify-email?token=...
+ * @desc Verify user's email address
+ * @access Public
+ * @query { token }
+ */
 authrouter.get("/verify-email", verifyEmail);
 
+/**
+ * @route POST /api/auth/login
+ * @desc Login user and return JWT token
+ * @access Public
+ * @body { email, password }
+ */
 authrouter.post("/login", loginValidator, loginUser);
+
+/**
+ * @route GET /api/auth/get-me
+ * @desc Get current logged in user's info
+ * @access Private
+ */
+
+authrouter.get("/get-me",);
 
 export default authrouter;
