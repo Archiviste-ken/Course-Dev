f00b112b1b2a0ea9f90d4280e2655ac34b2f39be
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-14 16:56:57 +0530

    style: format code for consistency in auth.api.js

--- Backend/Perplexity/frontend/src/features/auth/services/auth.api.js
+++ Backend/Perplexity/frontend/src/features/auth/services/auth.api.js
@@ -1,28 +1,22 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/auth',
-    withCredentials: true,
-})
+  baseURL: "http://localhost:3000/api/auth",
+  withCredentials: true,
+});
 
+export async function register({ username, email, password }) {
+  const response = await api.post("api/auth/register", {
+    username,
+    email,
+    password,
+  });
 
-
-export async function register({username,email,password}){
-
-    const response = await api.post('api/auth/register', {
-        username,
-        email,
-        password
-    })
-
-    return response.data
-
+  return response.data;
 }
 
+export async function login({ email, password }) {
+  const response = await api.post("/api/auth/login", { email, password });
 
-export async function login({email,password}){
-
-    const response = await api.post("/api/auth/login", {
-        
-    })
+  return response.data;
 }
