66f7dd1dca157f8f56bb9d8d4009b20941b15d1f
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-15 18:36:44 +0530

    feat: wrap Dashboard component with Protected route for authentication

--- Backend/Perplexity/frontend/src/app/app.routes.jsx
+++ Backend/Perplexity/frontend/src/app/app.routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
 import Dashboard from "../features/chats/pages/Dashboard";
+import Protected from "../features/auth/components/protected";
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,6 @@ export const router = createBrowserRouter([
   },
   {
     path: "/",
-    element: <Dashboard />,
+    element: <Protected><Dashboard /></Protected>,
   },
 ]);
