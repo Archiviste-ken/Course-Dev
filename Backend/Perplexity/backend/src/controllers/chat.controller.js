4158acdbfd40d01c31bcec80933d049814806d99
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-17 01:09:02 +0530

    feat: add Mistral AI integration and update chat response handling

--- Backend/Perplexity/backend/src/controllers/chat.controller.js
+++ Backend/Perplexity/backend/src/controllers/chat.controller.js
@@ -1,12 +1,17 @@
-import {generateResponse} from "../services/ai.services.js";
+import { generateResponse, generateChatTitle } from "../services/ai.services.js";
 
 export async function sendMessage(req, res) {
-
   const { message } = req.body;
 
-    const result = await generateResponse(message);
+  const result = await generateResponse(message);
 
-    res.json({ aiMessage: result });
-    
+  res.json({ aiMessage: result });
+}
 
+export async function generateChatTitle(req, res) {
+  const { message } = req.body;
+
+  const title = await generateChatTitle(message);
+
+  res.json({ chatTitle: title });
 }
--- Backend/Perplexity/backend/src/services/ai.services.js
+++ Backend/Perplexity/backend/src/services/ai.services.js
@@ -1,13 +1,35 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatMistralAI } from "@langchain/mistralai";
 import { HumanMessage } from "langchain";
 
-const model = new ChatGoogleGenerativeAI({
+const geminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
   apiKey: process.env.GEMINI_API_KEY,
+});
+
+const mistralModel = new ChatMistralAI({
+  model: "mistral-small",
+  apiKey: process.env.MISTRAL_API_KEY,
 });
 
 export async function generateResponse(message) {
   const response = await model.invoke([new HumanMessage(message)]);
 
   return response.text;
+}
+
+export async function generateChatTitle(message) {
+  const response = await mistralModel.invoke([
+    new SystemMessage(`You are a helpful assistant that generates concise and descriptive titles for chat conversations.
+      
+      User will provideyou with the first message of a chat conversation, and you will generate a title that captures the essence of the conversation in a 2-4 words. The title should be informative and engaging, giving users a clear idea of what the conversation is about.
+      
+      `),
+
+    new HumanMessage(
+      `Generate a title for a chat conversation based on the following message: "${message}" `,
+    ),
+  ]);
+  return response.text;
+  ;
 }
