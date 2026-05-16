8d95d99dbad9d0d3aeb2c07087e77be50eddfc6c
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-17 02:26:34 +0530

    feat: implement chat message creation and response handling in sendMessage function

--- Backend/Perplexity/backend/src/controllers/chat.controller.js
+++ Backend/Perplexity/backend/src/controllers/chat.controller.js
@@ -7,8 +7,20 @@ export async function sendMessage(req, res) {
   const { message } = req.body;
 
   const title = await generateChatTitle(message);
-  console.log(title);
+
   const result = await generateResponse(message);
+
+  const chat = await chatModel.create({
+    user: req.user._id,
+    title,
+  });
+
+  const aiMessage = await messageModel.create({
+    chat: chat._id,
+    content: result,
+    role: "ai",
+  });
+
 
   res.json({ aiMessage: result, title });
 }
