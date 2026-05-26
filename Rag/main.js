fb45f2545f0171b45dd854a95b575950e394ef51
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-26 18:53:32 +0530

    feat: implement Pinecone integration and add MistralAI embeddings support

--- Rag/main.js
+++ Rag/main.js
@@ -1,6 +1,15 @@
 import { PDFParse } from "pdf-parse";
 import fs from "fs";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import "dotenv/config";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { Pinecone } from "@pinecone-database/pinecone";
+
+const pc = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY,
+});
+
+const index = pc.index("rag");
 
 let dataBuffer = fs.readFileSync("story.pdf");
 
@@ -10,12 +19,36 @@ const parser = new PDFParse({
 
 const data = await parser.getText();
 
+const embeddings = new MistralAIEmbeddings({
+  apiKey: process.env.MISTRALAI_API_KEY,
+  Model: "mistral-embed",
+});
+
 console.log(data);
 
 const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 10,
+  chunkSize: 500,
   chunkOverlap: 0,
 });
 
 const chunks = await splitter.splitText(data.text);
-console.log(chunks);
+
+const docs = await Promise.all(
+  chunks.map(async (chunk) => {
+    const embedding = await embeddings.embedQuery(chunk);
+    return {
+      text: chunk,
+      embedding: embedding,
+    };
+  }),
+);
+
+const result = await index.upsert({
+    records: docs.map((doc, i) => ({
+        id: `doc-${i}`,
+        values: doc.embedding,
+        metadata: {
+            text: doc.text,
+        },
+    })),
+})
