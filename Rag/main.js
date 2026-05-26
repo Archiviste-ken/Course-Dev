68ce5dc291e699a4d73d2a087d36159780bd7a97
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-26 19:15:02 +0530

    refactor: comment out unused PDF parsing and text splitting code

--- Rag/main.js
+++ Rag/main.js
@@ -13,45 +13,56 @@ const pc = new Pinecone({
 const index = pc.index("rag-learn");
 
 let dataBuffer = fs.readFileSync("story.pdf");
+// const parser = new PDFParse({
+//   data: dataBuffer,
+// });
 
-const parser = new PDFParse({
-  data: dataBuffer,
-});
-
-const data = await parser.getText();
+// const data = await parser.getText();
 
 const embeddings = new MistralAIEmbeddings({
   apiKey: process.env.MISTRALAI_API_KEY,
   Model: "mistral-embed",
 });
 
-console.log(data);
+// console.log(data);
 
-const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 500,
-  chunkOverlap: 0,
-});
+// const splitter = new RecursiveCharacterTextSplitter({
+//   chunkSize: 500,
+//   chunkOverlap: 0,
+// });
 
-const chunks = await splitter.splitText(data.text);
+// const chunks = await splitter.splitText(data.text);
 
-const docs = await Promise.all(
-  chunks.map(async (chunk) => {
-    const embedding = await embeddings.embedQuery(chunk);
-    return {
-      text: chunk,
-      embedding: embedding,
-    };
-  }),
+// const docs = await Promise.all(
+//   chunks.map(async (chunk) => {
+//     const embedding = await embeddings.embedQuery(chunk);
+//     return {
+//       text: chunk,
+//       embedding: embedding,
+//     };
+//   }),
+// );
+
+// const result = await index.upsert({
+//   records: docs.map((doc, i) => ({
+//     id: `doc-${i}`,
+//     values: doc.embedding,
+//     metadata: {
+//       text: doc.text,
+//     },
+//   })),
+// });
+
+const queryEmbedding = await embeddings.embedQuery(
+  "How was the internship experience?",
 );
 
-const result = await index.upsert({
-  records: docs.map((doc, i) => ({
-    id: `doc-${i}`,
-    values: doc.embedding,
-    metadata: {
-      text: doc.text,
-    },
-  })),
+console.log(queryEmbedding);
+
+const result = await index.query({
+  vector: queryEmbedding,
+  topK: 2,
+  includeMetadata: true,
 });
 
-console.log(result);
+console.log(JSON.stringify(result));
