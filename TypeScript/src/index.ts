a46ab9fdb0219885d05c62d214645c194ac97c28
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-26 22:29:23 +0530

    refactor: update package.json scripts and clean up index.ts comments

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello");
+// console.log("Hello");
+
+/**
+ *  string, number, boolean, array, tuple, void, never
+ */
+
+// const a = "world";
+// const b = 5;
+
+// console.log(a / b);
+
+// const a: boolean = true;
+
+// console.log(a);
+
+
+const a: number[] = [1,2]
+
+a.push(3)
+
+console.log(a);
