17c3759913195efaa6f705bff495503be03c29ad
Shreyesh <shreyeshpachpor06@gmail.com>
2026-06-03 23:18:01 +0530

    feat: add initial HTML and JavaScript files for Redux introduction

---
+++ React/Redux_Intro/script.js
@@ -0,0 +1,21 @@
+let state = {
+  count: 0,
+};
+
+
+
+let prevState = state
+
+function increment() {
+  //mutating state directly
+    // state.count = state.count + 1;
+
+  //Not mutating state directly, instead creating a new object with the updated value // Since we are not using the dot notation, we can use the spread operator to copy the existing properties of the state object and then update the count property with the new value.
+
+  state = { count: state.count + 1 };
+}
+
+increment();
+console.log(state);
+
+// normal way to update state
