5565700630b51696ba212bf1812c98917cfbfd2e
Shreyesh <shreyeshpachpor06@gmail.com>
2026-06-04 12:47:19 +0530

    feat: refactor state structure and implement post increment logic in Redux example

--- React/Redux_Intro/script.js
+++ React/Redux_Intro/script.js
@@ -1,21 +1,34 @@
-let state = {
-  count: 0,
+let reduxState = {
+  post: 0,
   name: "Shreyesh Pachpor",
   age: 21,
 };
 
-let prevState = state;
+// let prevState = state;
 
-function increment() {
-  //mutating state directly
-  // state.count = state.count + 1;
+// function increment() {
+//   //mutating state directly
+//   // state.count = state.count + 1;
 
-  //Not mutating state directly, instead creating a new object with the updated value // Since we are not using the dot notation, we can use the spread operator to copy the existing properties of the state object and then update the count property with the new value.
+//   //Not mutating state directly, instead creating a new object with the updated value // Since we are not using the dot notation, we can use the spread operator to copy the existing properties of the state object and then update the count property with the new value.
 
-  state = { ...state, count: state.count + 1 };
+//   state = { ...state, count: state.count + 1 };
+// }
+
+// increment();
+// console.log(state);
+
+// // normal way to update state
+
+function Reducer (state) {
+  return { ...state, post: state.post + 1 };
 }
 
-increment();
-console.log(state);
+// What Redux will  Do //
 
-// normal way to update state
+
+reduxState = Reducer(reduxState, {type: 'post/increment'})
+
+
+
+
