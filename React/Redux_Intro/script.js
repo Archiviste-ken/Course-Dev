4d1d64c285278223c7779335857cacdccf7785af
Shreyesh <shreyeshpachpor06@gmail.com>
2026-06-05 01:34:22 +0530

    feat: refactor reducer function to handle post increment and decrement actions

--- React/Redux_Intro/script.js
+++ React/Redux_Intro/script.js
@@ -20,15 +20,23 @@ let reduxState = {
 
 // // normal way to update state
 
-function Reducer (state) {
-  return { ...state, post: state.post + 1 };
+function reducer(state, action) {
+  // action is an object that describes what happened, it has a type property that indicates the type of action being performed. The reducer function takes the current state and the action as arguments and returns a new state based on the action type.
+
+  if (action.type === "post/increment") {
+    return { ...state, post: state.post + 1 };
+  } else if (action.type === "post/decrement") {
+    return { ...state, post: state.post - 1 };
+  }
+
+  return state;
 }
 
-// What Redux will  Do //
+// What Redux will  Do
 
+reduxState = reducer(reduxState, { type: "post/increment" });
 
-reduxState = Reducer(reduxState, {type: 'post/increment'})
+console.log(reduxState);
 
-
-
-
+reduxState = reducer(reduxState, { type: "post/decrement" });
+console.log(reduxState);
