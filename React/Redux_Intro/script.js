8492614bf17da9c1b88992e173e3e715b0dc176c
Shreyesh <shreyeshpachpor06@gmail.com>
2026-06-05 20:35:38 +0530

    feat: initialize Redux setup with initial state and reducer logic

--- React/Redux_Intro/script.js
+++ React/Redux_Intro/script.js
@@ -1,4 +1,6 @@
-let reduxState = {
+import { createStore } from "redux";
+
+const initialState = {
   post: 0,
   name: "Shreyesh Pachpor",
   age: 21,
@@ -20,13 +22,15 @@ let reduxState = {
 
 // // normal way to update state
 
-function reducer(state, action) {
+function reducer(state = initialState, action) {
   // action is an object that describes what happened, it has a type property that indicates the type of action being performed. The reducer function takes the current state and the action as arguments and returns a new state based on the action type.
 
   if (action.type === "post/increment") {
     return { ...state, post: state.post + 1 };
   } else if (action.type === "post/decrement") {
     return { ...state, post: state.post - 1 };
+  } else if (action.type === "post/incrementBy") {
+    return { ...state, post: state.post + action.payload };
   }
 
   return state;
@@ -34,9 +38,19 @@ function reducer(state, action) {
 
 // What Redux will  Do
 
-reduxState = reducer(reduxState, { type: "post/increment" });
+// initialState = reducer(initialState, { type: "post/increment" });
 
-console.log(reduxState);
+// console.log(initialState);
 
-reduxState = reducer(reduxState, { type: "post/decrement" });
-console.log(reduxState);
+// initialState = reducer(initialState, { type: "post/decrement" });
+// console.log(initialState);
+
+// initialState = reducer(initialState, { type: "post/incrementBy", payload: 5 });
+// console.log(initialState);
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+
+reducer()
