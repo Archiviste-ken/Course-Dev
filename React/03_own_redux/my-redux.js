0b6370cd3371887b8caa6dd20c4f3da37ee2d546
Shreyesh <shreyeshpachpor06@gmail.com>
2026-06-07 10:10:54 +0530

    feat: implement custom Redux store and enhance reducer logic with action types

--- React/03_own_redux/my-redux.js
+++ React/03_own_redux/my-redux.js
@@ -1,4 +1,16 @@
-export function myCreateStore() {
-  const store = {};
+export function myCreateStore(reducer) {
+  let state;
+
+  const store = {
+    getState() {
+      return state;
+    },
+
+    dispatch(action) {
+      state = reducer(state,action)
+    },
+
+    subscribe() {},
+  };
   return store;
 }
