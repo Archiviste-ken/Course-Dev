ffdf976d40e93ed4b65f30fb0979f4ee90f49df4
Shreyesh <shreyeshpachpor06@gmail.com>
2026-06-09 22:35:58 +0530

    feat: implement cart item management with add, remove, and increase quantity actions

--- React/04_making-multiple-reducers/script.js
+++ React/04_making-multiple-reducers/script.js
@@ -6,8 +6,33 @@ const initialState = {
   cartItems: [],
   wishlist: [],
 };
+
+const CART_ADD_ITEM = "cart/addItem";
+const CART_REMOVE_ITEM = "cart/removeItem";
+const CART_ITEM_INCREASE_QUANTITY = " cart/increaseItemQuantity";
+
 function reducer(state = initialState, action) {
-  console.log(action);
+  // console.log(action);
+  // return state
+  console.log(state);
+
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.productId !== action.payload.productId,
+        ),
+      };
+      case CART_ITEM_INCREASE_QUANTITY:
+        return { ...state, cartItems: state.cartItems.map((cartItem)=> {
+
+        })}
+    default:
+      return state;
+  }
 }
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
@@ -15,6 +40,46 @@ const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
 console.log(store);
 
 store.dispatch({
-  type: "card/addItem",
+  type: CART_ADD_ITEM,
   payload: { productId: 1, quantity: 1 },
 });
+
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: {
+    productId: 12,
+    quantity: 1,
+  },
+});
+
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: {
+    productId: 2,
+    quantity: 1,
+  },
+});
+
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: {
+    productId: 5,
+    quantity: 1,
+  },
+});
+
+store.dispatch({
+  type: CART_REMOVE_ITEM,
+  payload: {
+    productId: 5,
+  },
+});
+
+store.dispatch({
+  type: CART_ITEM_INCREASE_QUANTITY,
+  payload: {
+    productId: 5,
+    
+  },
+});
+
